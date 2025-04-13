678bacde3900a2c71f8728a20cdb6e52af6e7466
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 18:59:15 +0200

    Update erik.cpp

--- components/erik/erik.cpp
+++ components/erik/erik.cpp
@@ -5,8 +5,8 @@ namespace esphome {
 namespace erik {
 
 void ErikComponent::setup() {
-  auto cfg = m5.config();
-  M5.begin(cfg);
+//  auto cfg = m5.config();
+//  M5.begin(cfg);
 
   display_.begin();
   display_.setRotation(0);
