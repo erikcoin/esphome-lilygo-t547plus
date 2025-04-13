52b53baffa4404f7cd7c2e3d0e746b1524476578
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 14:29:49 +0200

    Update display.py

--- components/m5display/display.py
+++ components/m5display/display.py
@@ -1,53 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import display
-from esphome.const import (
-    CONF_ID,
-    CONF_LAMBDA,
-    CONF_PAGES,
-)
-from esphome.const import __version__ as ESPHOME_VERSION
+from esphome.const import CONF_ID
 
-DEPENDENCIES = ["esp32"]
+m5display_ns = cg.esphome_ns.namespace('m5display')
+M5Display = m5display_ns.class_('M5Display', cg.Component, display.DisplayBuffer)
 
-CONF_GREYSCALE = "greyscale"
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(M5Display),
+}).extend(cv.COMPONENT_SCHEMA)
 
-
-t547_ns = cg.esphome_ns.namespace("t547")
-T547 = t547_ns.class_(
-    "T547", cg.PollingComponent, display.DisplayBuffer
-)
-
-CONFIG_SCHEMA = cv.All(
-    display.FULL_DISPLAY_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(T547),
-            cv.Optional(CONF_GREYSCALE, default=False): cv.boolean,
-        }
-    )
-    .extend(cv.polling_component_schema("5s")),
-    cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
-    cv.only_with_arduino,
-)
-
-
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-
-    if cv.Version.parse(ESPHOME_VERSION) < cv.Version.parse("2023.12.0"):
-        await cg.register_component(var, config)
-    await display.register_display(var, config)
-
-    if CONF_LAMBDA in config:
-        lambda_ = await cg.process_lambda(
-            config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
-        )
-        cg.add(var.set_writer(lambda_))
-
-    cg.add(var.set_greyscale(config[CONF_GREYSCALE]))
-
-    cg.add_build_flag("-DBOARD_HAS_PSRAM")
-
-    cg.add_library("Wire", version="2.0.0")  # required by LilyGoEPD47
-    cg.add_library("LilyGoEPD47", repository="https://github.com/erikcoin/LilyGo-EPD47", version="None")
+    yield cg.register_component(var, config)
+    yield display.register_display(var, config)
