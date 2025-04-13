03a93a84f3cb10486c464abf1f959da255357611
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 20:58:53 +0200

    Update __init__.py

--- components/erik/__init__.py
+++ components/erik/__init__.py
@@ -1,21 +1,23 @@
+from esphome import automation
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import light
+from esphome.components import display, text_sensor
 from esphome.const import CONF_ID
 
 erik_ns = cg.esphome_ns.namespace("erik")
-ErikComponent = erik_ns.class_("ErikComponent", cg.PollingComponent)
+ErikDisplay = erik_ns.class_("ErikDisplay", display.DisplayBuffer)
 
-CONF_LIGHT = "light"
+CONF_LIGHT_STATE = "light_state"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(ErikComponent),
-    cv.Required(CONF_LIGHT): cv.use_id(light.LightState),
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(ErikDisplay),
+    cv.Required(CONF_LIGHT_STATE): cv.use_id(text_sensor.TextSensor),
 }).extend(cv.polling_component_schema("1s"))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
+    yield display.register_display(var, config)
     yield cg.register_component(var, config)
 
-    lt = yield cg.get_variable(config[CONF_LIGHT])
-    cg.add(var.set_light(lt))
+    light_state = yield cg.get_variable(config[CONF_LIGHT_STATE])
+    cg.add(var.set_light_state(light_state))
