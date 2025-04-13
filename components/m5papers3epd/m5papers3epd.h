71cd54d30aa9fcc2d67cb10250fb41dcc9fd9c85
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 16:12:38 +0200

    Update m5papers3epd.h

--- components/m5papers3epd/m5papers3epd.h
+++ components/m5papers3epd/m5papers3epd.h
@@ -1,1 +1,23 @@
 
+#pragma once
+
+#include "esphome/core/component.h"
+#include <epdiy.h>
+#include <M5Unified.h>
+
+
+namespace esphome {
+namespace m5papers3epd {
+
+class M5PaperS3EPD : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+
+ protected:
+  M5GFX display_;
+  void draw_demo();
+};
+
+}  // namespace m5papers3epd
+}  // namespace esphome
