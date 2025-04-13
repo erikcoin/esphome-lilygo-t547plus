956da25b043d263f37e0342ab7bccef7255a9b6d
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 15:39:39 +0200

    Update m5display.cpp

--- components/m5display/m5display.cpp
+++ components/m5display/m5display.cpp
@@ -1,20 +1,4 @@
 #include "esphome.h"
-#include <epdiy.h>
-#include <M5Unified.h>
+#include "m5display.h"  // Include the header where the class is defined
 
-class M5PaperDisplay : public Component {
- public:
-  void setup() override {
-    // Initialize M5 Paper display
-    m5.begin();
-    m5.lcd.setTextColor(TFT_WHITE);
-    m5.lcd.setTextSize(2);
-    m5.lcd.setCursor(10, 10);
-    m5.lcd.print("Hello, M5 Paper!");
-  }
-
-  void loop() override {
-    // Your display update logic goes here
-    // For example, refreshing text, updating screen, etc.
-  }
-};
+// No need to redefine the class here
