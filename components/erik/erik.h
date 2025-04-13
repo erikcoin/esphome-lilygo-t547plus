055401cc8ed32ac5eb5c503c59226bdfeb24b499
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 21:25:35 +0200

    Update erik.h

--- components/erik/erik.h
+++ components/erik/erik.h
@@ -3,19 +3,19 @@
 #include "esphome/core/component.h"
 #include "esphome/components/display/display_buffer.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include <epdiy.h>
+#include "esphome/api.h"  // Include API for calls
 #include <M5Unified.h>
 
 namespace esphome {
 namespace erik {
 
-class ErikDisplay : public display::DisplayBuffer, public Component {
+class ErikDisplay : public display::DisplayBuffer, public PollingComponent {
  public:
   void set_light_state(text_sensor::TextSensor *state) { this->light_state_ = state; }
 
   void setup() override;
   void update() override;
-  void draw_display() override {}
+  void draw_display() override;
 
   int get_width() override { return 540; }
   int get_height() override { return 960; }
