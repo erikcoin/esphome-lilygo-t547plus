b7f047f992eac1b318bcd7ae8835ef535c722cb6
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 16:40:11 +0200

    Create erik.cpp

---
+++ components/erik/erik.cpp
@@ -0,0 +1,25 @@
+#include "erik.h"
+
+namespace esphome {
+namespace erik {
+
+void ErikComponent::setup() {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+
+  display_.begin();
+  display_.setRotation(0);
+  display_.fillScreen(TFT_WHITE);
+  display_.display();
+}
+
+void ErikComponent::update() {
+  display_.fillScreen(TFT_WHITE);
+  display_.setTextColor(TFT_BLACK);
+  display_.setTextSize(size_);
+  display_.drawString(text_.c_str(), x_, y_);
+  display_.display();  // Push to EPD
+}
+
+}  // namespace erik
+}  // namespace esphome
