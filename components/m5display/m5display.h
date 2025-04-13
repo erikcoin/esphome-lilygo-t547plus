74476dd38b86086b3de82c8aedb503169d503f0d
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 15:25:26 +0200

    Update m5display.h

--- components/m5display/m5display.h
+++ components/m5display/m5display.h
@@ -1,40 +1,11 @@
+#pragma once
+
 #include "esphome.h"
+#include <epdiy.h>
 #include <M5Unified.h>
 
-class M5UnifiedDisplay : public PollingComponent, public DisplayBuffer {
+class M5PaperDisplay : public Component {
  public:
-  M5UnifiedDisplay() : PollingComponent(1000) {}
-
-  void setup() override {
-    auto cfg = M5.config();
-    M5.begin(cfg);
-    M5.Display.fillScreen(BLACK);
-    M5.Display.setTextColor(WHITE);
-    M5.Display.setTextSize(2);
-  }
-
-  void update() override {
-    this->do_display();
-  }
-
-  void draw_absolute_pixel_internal(int x, int y, Color color) override {
-    M5.Display.drawPixel(x, y, color.raw_888());
-  }
-
-  int get_width_internal() override { return M5.Display.width(); }
-  int get_height_internal() override { return M5.Display.height(); }
-
-  void display() override {
-    M5.Display.startWrite();
-    DisplayBuffer::display();
-    M5.Display.endWrite();
-  }
-
-  void fill(Color color) override {
-    M5.Display.fillScreen(color.raw_888());
-  }
-
-  void draw_pixel_at(int x, int y, Color color) {
-    M5.Display.drawPixel(x, y, color.raw_888());
-  }
+  void setup() override;
+  void loop() override;
 };
