7e9adad8d9a8323027b35b88d48e5645312fb31e
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 14:34:10 +0200

    Update display.py

--- components/m5display/display.py
+++ components/m5display/display.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import display
 from esphome.const import CONF_ID
 
-m5display_ns = cg.esphome_ns.namespace('m5display')
+m5gfx_ns = cg.esphome_ns.namespace('m5gfx')
 M5Display = m5display_ns.class_('M5Display', cg.Component, display.DisplayBuffer)
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
