4efb52bf1422ec38d547401453ece8c9971ada63
erikcoin <erikdijkstra@hotmail.com>
2025-04-13 21:39:31 +0200

    Update erik.cpp

--- components/erik/erik.cpp
+++ components/erik/erik.cpp
@@ -12,7 +12,7 @@ void ErikDisplay::setup() {
 void ErikDisplay::update() {
   M5.update();
 
-  bool is_touched = M5.Touch.isPressed();
+  bool is_touched = M5.Touch.isTouched();
   if (is_touched && !last_touch_state_) {
     if (this->touch_callback_) {
       this->touch_callback_();
@@ -21,20 +21,20 @@ void ErikDisplay::update() {
   last_touch_state_ = is_touched;
 }
 
-void ErikDisplay::draw_display() {
+void ErikDisplay::update_display() {
   M5.Display.fillScreen(TFT_BLACK);
   M5.Display.setCursor(40, 50);
   M5.Display.setTextColor(TFT_WHITE);
   M5.Display.setTextSize(2);
   M5.Display.print("Bibliotheeklamp");
 
-  draw_button_(true);  // Always ON as example; you can add state binding later
+  draw_button_(true);  // hardcoded ON
 }
 
 void ErikDisplay::draw_button_(bool state) {
   int x = 100, y = 150, w = 200, h = 60;
-
   uint32_t color = state ? TFT_GREEN : TFT_RED;
+
   M5.Display.fillRoundRect(x, y, w, h, 10, color);
   M5.Display.setTextColor(TFT_BLACK);
   M5.Display.setCursor(x + 30, y + 20);
