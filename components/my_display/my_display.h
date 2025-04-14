7dae669ce1422bc627557e0e414e3463113938c8
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 18:40:14 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -6,17 +6,17 @@
 #include <M5GFX.h>
 
 namespace esphome {
-namespace my_display {
+namespace my_display {  // 👈 MUST MATCH display.py namespace
 
-class MyDisplay : public display::DisplayBuffer,  {
+class MyDisplay : public esphome::display::DisplayBuffer, public esphome::Component {
  public:
   void setup() override;
   void update() override;
   void dump_config() override;
 
  protected:
-  void draw_absolute_pixel_internal(int x, int y, Color color) override;
-  int get_width_internal() override { return 960; }  // Adjust to your e-paper size
+  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
+  int get_width_internal() override { return 960; }
   int get_height_internal() override { return 540; }
 
  private:
