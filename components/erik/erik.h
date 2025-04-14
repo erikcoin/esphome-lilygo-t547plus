e16398a9a5bc1184244a13a613f5215c2e5af9f6
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 16:05:44 +0200

    Update erik.h

--- components/erik/erik.h
+++ components/erik/erik.h
@@ -3,17 +3,17 @@
 #include "esphome/core/component.h"
 #include "esphome/components/display/display_buffer.h"
 #include "esphome/components/display/display_color_utils.h"
-#include <epdiy.h>
+<include <epdiy.h>
 #include <M5GFX.h>
 
 namespace esphome {
 namespace erik {
 
-class Erik : public Component, public display::DisplayBuffer {
+class Erik : public esphome::Component, public esphome::display::DisplayBuffer {
  public:
   void setup() override;
   void update() override;
-  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
 
   int get_width() override { return 960; }
   int get_height() override { return 540; }
