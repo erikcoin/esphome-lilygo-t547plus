4833f261fd5801efa3890cceba618c37a40dd94b
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 16:54:42 +0200

    Update erik.cpp

--- components/erik/erik.cpp
+++ components/erik/erik.cpp
@@ -1,22 +1,88 @@
 #include "erik.h"
+#include "esphome/core/log.h"
+#include "esphome/core/application.h"
+#include "esphome/core/helpers.h"
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
+
+#include <esp32-hal-gpio.h>
 
 namespace esphome {
 namespace erik {
 
+static const char *const TAG = "erik";
+
 void Erik::setup() {
-  display_.begin();
-  display_.setRotation(1);
-  display_.fillScreen(TFT_WHITE);
+  ESP_LOGV(TAG, "Initialize called");
+  epd_init();
+  uint32_t buffer_size = this->get_buffer_length_();
+
+  if (this->buffer_ != nullptr) {
+    free(this->buffer_);  // NOLINT
+  }
+
+  this->buffer_ = (uint8_t *) ps_malloc(buffer_size);
+
+  if (this->buffer_ == nullptr) {
+    ESP_LOGE(TAG, "Could not allocate buffer for display!");
+    this->mark_failed();
+    return;
+  }
+
+  memset(this->buffer_, 0xFF, buffer_size);
+  ESP_LOGV(TAG, "Initialize complete");
+}
+
+float Erik::get_setup_priority() const { return setup_priority::PROCESSOR; }
+size_t Erik::get_buffer_length_() {
+    return this->get_width_internal() * this->get_height_internal() / 2;
 }
 
 void Erik::update() {
-  this->do_update_();  // ESPHome buffer tekenen
+  this->do_update_();
+  this->display();
 }
 
-void Erik::draw_absolute_pixel_internal(int x, int y, Color color) {
-  uint16_t col = color.is_on() ? TFT_BLACK : TFT_WHITE;
-  display_.drawPixel(x, y, col);
+void HOT Erik::draw_absolute_pixel_internal(int x, int y, Color color) {
+  if (x >= this->get_width_internal() || y >= this->get_height_internal() || x < 0 || y < 0)
+    return;
+  uint8_t gs = 255 - ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
+  epd_draw_pixel(x, y, gs, this->buffer_);
 }
 
-}  // namespace erik
+void Erik::dump_config() {
+  LOG_DISPLAY("", "T547", this);
+  LOG_UPDATE_INTERVAL(this);
+}
+
+void Erik::eink_off_() {
+  ESP_LOGV(TAG, "Eink off called");
+  if (panel_on_ == 0)
+    return;
+  epd_poweroff();
+  panel_on_ = 0;
+}
+
+void Erik::eink_on_() {
+  ESP_LOGV(TAG, "Eink on called");
+  if (panel_on_ == 1)
+    return;
+  epd_poweron();
+  panel_on_ = 1;
+}
+
+void Erik::display() {
+  ESP_LOGV(TAG, "Display called");
+  uint32_t start_time = millis();
+
+  epd_poweron();
+  epd_clear();
+  epd_draw_grayscale_image(epd_full_screen(), this->buffer_);
+  epd_poweroff();
+
+  ESP_LOGV(TAG, "Display finished (full) (%ums)", millis() - start_time);
+}
+
+}  // namespace T547
 }  // namespace esphome
+
+#endif  // USE_ESP32_FRAMEWORK_ARDUINO
