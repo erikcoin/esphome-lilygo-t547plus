872c6eec6a317242ba71bfe095c047968afa3b33
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 16:06:27 +0200

    Update display.py

--- components/erik/display.py
+++ components/erik/display.py
@@ -4,12 +4,11 @@ from esphome.components import display
 from esphome.const import CONF_ID
 
 erik_ns = cg.esphome_ns.namespace('erik')
-
-Erik = erik_ns.class_("Erik", cg.Component, display.DisplayBuffer)
+Erik = erik_ns.class_('Erik', cg.Component, display.DisplayBuffer)
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Erik),
-}).extend(cv.polling_component_schema('5s'))
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
