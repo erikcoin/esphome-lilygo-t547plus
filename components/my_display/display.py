1f6cd6b9b958881576b51bdac3cf1f10795a324f
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 18:12:54 +0200

    Update display.py

--- components/my_display/display.py
+++ components/my_display/display.py
@@ -1,1 +1,20 @@
 
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import display
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@your-github-username"]  # Optional
+DEPENDENCIES = ["display"]
+
+my_display_ns = cg.esphome_ns.namespace("my_display")
+MyDisplay = my_display_ns.class_("MyDisplay", cg.Component, display.DisplayBuffer)
+
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(MyDisplay),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await display.register_display(var, config)
