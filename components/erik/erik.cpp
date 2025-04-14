344e146ebfb533c3a91d34e8c6758c349fb7fda2
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 15:51:24 +0200

    Update erik.cpp

--- components/erik/erik.cpp
+++ components/erik/erik.cpp
@@ -1,61 +1,22 @@
 #include "erik.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
 namespace erik {
 
-void ErikDisplay::setup() {
-  M5.begin();
-  M5.Display.setRotation(3);
-  draw_screen_();
+void Erik::setup() {
+  display_.begin();
+  display_.setRotation(1);  // afhankelijk van je schermoriëntatie
+  display_.fillScreen(TFT_WHITE);
 }
 
-void ErikDisplay::update() {
-  M5.update();
-
-  auto touch = M5.Touch.getDetail();
-  bool is_touched = touch.wasPressed();
-
-  if (is_touched && !last_touch_state_ && is_touch_inside_button_()) {
-    if (this->touch_callback_) {
-      this->touch_callback_();
-    }
-  }
-
-  last_touch_state_ = is_touched;
+void Erik::update() {
+  this->do_update_();  // zorgt ervoor dat ESPHome intern het buffer opnieuw tekent
+  display_.display();  // in sommige m5gfx drivers niet nodig, afhankelijk van implementatie
 }
 
-void ErikDisplay::show_state(bool on) {
-  light_on_ = on;
-  draw_screen_();
-}
-
-void ErikDisplay::draw_screen_() {
-  M5.Display.fillScreen(TFT_BLACK);
-  M5.Display.setTextColor(TFT_WHITE);
-  M5.Display.setTextSize(2);
-  M5.Display.setCursor(30, 30);
-  M5.Display.print("Bibliotheeklamp");
-
-  // Draw toggle button
-  int x = 100, y = 150, w = 200, h = 60;
-  uint32_t color = light_on_ ? TFT_GREEN : TFT_RED;
-
-  M5.Display.fillRoundRect(x, y, w, h, 10, color);
-  M5.Display.setTextColor(TFT_BLACK);
-  M5.Display.setCursor(x + 40, y + 20);
-  M5.Display.print(light_on_ ? "Turn Off" : "Turn On");
-}
-
-bool ErikDisplay::is_touch_inside_button_() {
-  m5::touch_detail_t t = M5.Touch.getDetail();
-  int x = t.x;
-  int y = t.y;
-
-  // Button bounds
-  int bx = 100, by = 150, bw = 200, bh = 60;
-
-  return (x >= bx && x <= (bx + bw) && y >= by && y <= (by + bh));
+void Erik::draw_absolute_pixel_internal(int x, int y, Color color) {
+  uint16_t col = color.is_on() ? TFT_WHITE : TFT_BLACK;
+  display_.drawPixel(x, y, col);
 }
 
 }  // namespace erik
