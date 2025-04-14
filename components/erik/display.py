5400e2ecd2e812e18d694b47a602d677bac6add7
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 15:52:41 +0200

    Update display.py

--- components/erik/display.py
+++ components/erik/display.py
@@ -3,19 +3,14 @@ import esphome.config_validation as cv
 from esphome.components import display
 from esphome.const import CONF_ID
 
-erik_ns = cg.esphome_ns.namespace("erik")
-ErikDisplay = erik_ns.class_("ErikDisplay", display.DisplayBuffer)
-
-CONF_ON_TOUCH = "on_touch"
+erik_ns = cg.esphome_ns.namespace('erik')
+Erik = erik_ns.class_('Erik', cg.PollingComponent, display.DisplayBuffer)
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(ErikDisplay),
-    cv.Optional(CONF_ON_TOUCH): cv.returning_lambda,
-}).extend(cv.polling_component_schema("1s"))
+    cv.GenerateID(): cv.declare_id(Erik),
+}).extend(cv.polling_component_schema('5s'))
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield display.register_display(var, config)
-
-    if touch_lambda := config.get(CONF_ON_TOUCH):
-        cg.add(var.set_touch_callback(touch_lambda))
+    await cg.register_component(var, config)
+    await display.register_display(var, config)
