8c8d8a0cc6406ca9c9072a335cd5527ec091cf3c
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 18:21:11 +0200

    Update display.py

--- components/my_display/display.py
+++ components/my_display/display.py
@@ -8,9 +8,11 @@ CODEOWNERS = ["@your-github-username"]  # Optional
 DEPENDENCIES = ["display"]
 
 my_display_ns = cg.esphome_ns.namespace("my_display")
-MyDisplay = my_display_ns.class_("MyDisplay", cg.PollingComponent, display.DisplayBuffer)
 
 
+MyDisplay = my_display_ns.class_(
+    "MyDisplay", cg.PollingComponent, display.DisplayBuffer
+)
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MyDisplay),
 }).extend(cv.COMPONENT_SCHEMA)
