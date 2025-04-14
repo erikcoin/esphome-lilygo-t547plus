0526f7bae0a1aa1b5912122216eee8112e3e7e7b
erikcoin <erikdijkstra@hotmail.com>
2025-04-14 16:11:06 +0200

    Update erik.cpp

--- components/erik/erik.cpp
+++ components/erik/erik.cpp
@@ -5,17 +5,16 @@ namespace erik {
 
 void Erik::setup() {
   display_.begin();
-  display_.setRotation(1);  // afhankelijk van je schermoriëntatie
+  display_.setRotation(1);
   display_.fillScreen(TFT_WHITE);
 }
 
 void Erik::update() {
-  this->do_update_();  // zorgt ervoor dat ESPHome intern het buffer opnieuw tekent
-  display_.display();  // in sommige m5gfx drivers niet nodig, afhankelijk van implementatie
+  this->do_update_();  // ESPHome buffer tekenen
 }
 
 void Erik::draw_absolute_pixel_internal(int x, int y, Color color) {
-  uint16_t col = color.is_on() ? TFT_WHITE : TFT_BLACK;
+  uint16_t col = color.is_on() ? TFT_BLACK : TFT_WHITE;
   display_.drawPixel(x, y, col);
 }
 
