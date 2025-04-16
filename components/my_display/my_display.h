0b50b0dc1121dca7bb539846cfac4ab194791eb0
erikcoin <erikdijkstra@hotmail.com>
2025-04-16 18:14:26 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -1,15 +1,33 @@
-#pragma once
 #include "esphome.h"
-#include <epdiy.h>
 #include <M5GFX.h>
+#include <epd_driver.h>
 
-class MyEpaperDisplay : public display::DisplayBuffer {
+class MyEpaperDisplay : public esphome::display::DisplayBuffer {
  public:
-  MyEpaperDisplay();
-  void setup() override;
-  void update() override;
-  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
-  void fill(esphome::Color color) override;
+  MyEpaperDisplay() : DisplayBuffer(960, 540) {}
+
+  void setup() override {
+    epd_init();
+    gfx.init();
+    gfx.setRotation(1);
+    gfx.setTextColor(EPD_BLACK);
+    gfx.setTextSize(1);
+    gfx.fillScreen(EPD_WHITE);
+    gfx.display();
+  }
+
+  void update() override {
+    this->do_draw_();  // ESPHome lambda draw-calls
+    gfx.display();     // Refresh e-paper
+  }
+
+  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override {
+    gfx.drawPixel(x, y, color.is_on() ? EPD_BLACK : EPD_WHITE);
+  }
+
+  void fill(esphome::Color color) override {
+    gfx.fillScreen(color.is_on() ? EPD_BLACK : EPD_WHITE);
+  }
 
  protected:
   M5GFX gfx;
