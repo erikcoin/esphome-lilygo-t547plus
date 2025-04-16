3a258bafc9fd865dee72185c922c82e82eafd663
erikcoin <erikdijkstra@hotmail.com>
2025-04-16 18:06:21 +0200

    Update display.py

--- components/my_display/display.py
+++ components/my_display/display.py
@@ -1,13 +1,14 @@
 import esphome.codegen as cg
+import esphome.components.display as display
+from esphome.components import font
 import esphome.config_validation as cv
-from esphome.components import display
 from esphome.const import CONF_ID
 
-my_display_ns = cg.esphome_ns.namespace("my_display")  # 👈 must match C++ namespace
-MyDisplay = my_display_ns.class_("MyDisplay", cg.Component, display.DisplayBuffer)  # 👈 class name + inheritance
+my_epaper_ns = cg.esphome_ns.namespace("my_epaper_display")
+MyEpaperDisplay = my_epaper_ns.class_("MyEpaperDisplay", cg.Component, display.DisplayBuffer)
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MyDisplay),
+    cv.GenerateID(): cv.declare_id(MyEpaperDisplay),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
