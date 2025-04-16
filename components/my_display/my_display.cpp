6472ebdb75ae8f34c453c05f061f701066aa05bf
erikcoin <erikdijkstra@hotmail.com>
2025-04-16 18:27:16 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -1,1 +1,27 @@
 #include "my_display.h"
+#include <epd_driver.h>
+
+MyEpaperDisplay::MyEpaperDisplay() : DisplayBuffer(960, 540) {}
+
+void MyEpaperDisplay::setup() {
+  epd_init();
+  gfx.init();
+  gfx.setRotation(1);
+  gfx.setTextColor(EPD_BLACK);
+  gfx.setTextSize(1);
+  gfx.fillScreen(EPD_WHITE);
+  gfx.display();
+}
+
+void MyEpaperDisplay::update() {
+  this->do_draw_();
+  gfx.display();
+}
+
+void MyEpaperDisplay::draw_absolute_pixel_internal(int x, int y, esphome::Color color) {
+  gfx.drawPixel(x, y, color.is_on() ? EPD_BLACK : EPD_WHITE);
+}
+
+void MyEpaperDisplay::fill(esphome::Color color) {
+  gfx.fillScreen(color.is_on() ? EPD_BLACK : EPD_WHITE);
+}
