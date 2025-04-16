69c1e3a8d3d7863dd5c7dfc6b17f311ee65dc1be
erikcoin <erikdijkstra@hotmail.com>
2025-04-16 19:20:45 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -1,30 +1,29 @@
 #include "my_display.h"
-#include <epdiy.h>
-#include <M5GFX.h> // M5GFX voor display functies
 
-MyEpaperDisplay::MyEpaperDisplay() //: DisplayBuffer(960, 540) {} // Resolutie van het ED047TC1 scherm
+MyEpaperDisplay::MyEpaperDisplay() {}
 
 void MyEpaperDisplay::setup() {
-  // Initieer de M5GFX bibliotheek
+  this->set_width(960);
+  this->set_height(540);
+  this->set_writer([](esphome::display::Display *d) {});
+
   gfx.begin();
-  gfx.setRotation(1); // Zet de rotatie van het scherm (pas aan naar behoefte)
-  gfx.setTextColor(TFT_BLACK); // Zet de tekstkleur
-  gfx.setTextSize(1); // Zet de tekstgrootte
-  gfx.fillScreen(TFT_WHITE); // Vul het scherm met wit
-  gfx.display(); // Werk de weergave bij
+  gfx.setRotation(1);
+  gfx.setTextColor(BLACK);
+  gfx.setTextSize(1);
+  gfx.fillScreen(WHITE);
+  gfx.display();
 }
 
 void MyEpaperDisplay::update() {
-  this->do_draw_(); // Teken de pixeldata naar het scherm
-  gfx.display(); // Werk het scherm bij
+  // Niets nodig als je lambda in YAML gebruikt
+  gfx.display();
 }
 
 void MyEpaperDisplay::draw_absolute_pixel_internal(int x, int y, esphome::Color color) {
-  // Teken individuele pixels, afhankelijk van de kleur
-  gfx.drawPixel(x, y, color.is_on() ? TFT_BLACK : TFT_WHITE);
+  gfx.drawPixel(x, y, color.is_on() ? BLACK : WHITE);
 }
 
 void MyEpaperDisplay::fill(esphome::Color color) {
-  // Vul het scherm met de opgegeven kleur
-  gfx.fillScreen(color.is_on() ? TFT_BLACK : TFT_WHITE);
+  gfx.fillScreen(color.is_on() ? BLACK : WHITE);
 }
