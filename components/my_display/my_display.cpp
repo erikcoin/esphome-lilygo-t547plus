e758b711115732d6163781a7b9feb01c09033748
erikcoin <erikdijkstra@hotmail.com>
2025-04-19 10:21:38 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -14,7 +14,7 @@ void MyEpaperDisplay::setup() {
   //gfx.clear();              // wist interne framebuffer (optioneel)
   //gfx.fillScreen(TFT_BLACK); // teken volledig wit
   //gfx.display();
-  gfx.fillScreen(TFT_WHITE); // teken volledig wit
+  gfx.fillScreen(TFT_BLACK); // teken volledig wit
   gfx.display();
 }
 
