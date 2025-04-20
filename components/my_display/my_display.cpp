bddcc9665e25521a133335aece785646b5e2bb85
erikcoin <erikdijkstra@hotmail.com>
2025-04-20 09:57:22 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -12,21 +12,21 @@ void MyEpaperDisplay::setup() {
   gfx.begin();
   gfx.setRotation(0);
   // Init canvas met juiste grootte en 1-bit kleur
-  canvas.setColorDepth(1);  // 1-bit zwart/wit. Of 4 voor grayscale
-  canvas.createSprite(get_width_internal(), get_height_internal());
-  canvas.setFont(&fonts::Font0);  // optioneel, fallback
+  //canvas.setColorDepth(1);  // 1-bit zwart/wit. Of 4 voor grayscale
+  //canvas.createSprite(get_width_internal(), get_height_internal());
+  //canvas.setFont(&fonts::Font0);  // optioneel, fallback
   
-  canvas.setTextColor(TFT_BLACK, TFT_WHITE);
-  canvas.fillScreen(TFT_WHITE);
+ // canvas.setTextColor(TFT_BLACK, TFT_WHITE);
+ // canvas.fillScreen(TFT_WHITE);
   // Push canvas naar display
-  gfx.startWrite();
-  canvas.pushSprite(0, 0);  // Nu wordt de canvas-inhoud ook echt weergegeven
-  gfx.endWrite();
+//  gfx.startWrite();
+//  canvas.pushSprite(0, 0);  // Nu wordt de canvas-inhoud ook echt weergegeven
+//  gfx.endWrite();
 
   //gfx.display();  // Ververs scherm met huidige framebuffer
   //vanaf hier werkt het zonder canvas
   //gfx.clear();              // wist interne framebuffer (optioneel)
-  //gfx.fillScreen(TFT_BLACK); // teken volledig wit
+  gfx.fillScreen(TFT_BLACK); // teken volledig wit
   gfx.display();
 }
 
