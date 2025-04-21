f654123be45aa605c0c0b80a20c5be45b8eb292a
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 16:43:33 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -1,30 +1,25 @@
+
 #include "my_display.h"
 
 namespace esphome {
 namespace my_display {
 
-void MyEpaperDisplay::setup() {
-  gfx.init();  // Initialiseer M5GFX
-  gfx.setRotation(0);
-  gfx.setEpdMode(epd_mode_t::epd_quality);  // evt. relevant voor je e-paper
-  gfx.fillScreen(TFT_WHITE);                // Maak scherm wit bij start
-
-  // Canvas/Sprite maken
-  LGFX_Sprite canvas(&gfx);                 // Koppel canvas aan het display
-  canvas.createSprite(250, 122);            // Pas formaat aan aan je scherm
-  canvas.fillScreen(TFT_WHITE);             // Achtergrondkleur
-
-  canvas.setTextColor(TFT_BLACK);
-  canvas.setTextSize(2);
-  canvas.drawString("Hallo ESPHome!", 10, 20);  // Teken tekst op canvas
-
-  canvas.pushSprite(0, 0);                  // Teken canvas op scherm
-  canvas.deleteSprite();                    // Opruimen
-  ESP_LOGI("my_display", "Scherm wit gemaakt bij opstart.");
+void MyDisplay::setup() {
+  gfx_.begin();
+  canvas_.setColorDepth(1);
+  canvas_.createSprite(gfx_.width(), gfx_.height());
+  canvas_.fillScreen(TFT_WHITE);
+  canvas_.pushSprite(0, 0);
 }
 
-void MyEpaperDisplay::update() {
-  // geen rendering via lambda nodig hier
+void MyDisplay::update() {
+  canvas_.fillScreen(TFT_WHITE);
+  canvas_.fillTriangle(60, 60, 120, 180, 180, 60, TFT_BLACK);
+  canvas_.pushSprite(0, 0);
+}
+
+void MyDisplay::draw_absolute_pixel_internal(int x, int y, int color) {
+  canvas_.drawPixel(x, y, color);
 }
 
 }  // namespace my_display
