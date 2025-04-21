4ec200bf256155c1d950d0b18139c1725d926fc9
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 09:18:59 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -2,7 +2,8 @@
 #include "esphome/core/log.h"
 #include "esphome/core/component.h"
 #include "esphome/components/display/display.h"
-//namespace esphome {
+
+namespace esphome {
 namespace my_display22 {
 
 MyEpaperDisplay::MyEpaperDisplay() {}
@@ -11,19 +12,20 @@ void MyEpaperDisplay::setup() {
   bool ok = gfx.begin();
   ESP_LOGD("my_display", "gfx.begin() resultaat: %s", ok ? "OK" : "MISLUKT");
   //gfx.begin();
-  gfx.setRotation(0);
-  gfx.fillScreen(TFT_WHITE);
-  gfx.display();
+  this.>gfx.setRotation(0);
+  this.>gfx.fillScreen(TFT_WHITE);
+  this.>gfx.display();
+  ESP_LOGD("my_display", "gfx.display() resultaat: %s", ok ? "OK" : "MISLUKT");
 }
 
 void MyEpaperDisplay::update() {
   ESP_LOGD("my_display", "TEST: update wordt aangeroepen");
 
-  gfx.fillScreen(TFT_WHITE);
-  gfx.setTextColor(TFT_BLACK);
-  gfx.setCursor(20, 20);
-  gfx.print("DIT IS EEN TEST OM TEI ZIEN OF IE HET GOED OET");
-  gfx.display();
+  //gfx.fillScreen(TFT_WHITE);
+  //gfx.setTextColor(TFT_BLACK);
+  //gfx.setCursor(20, 20);
+  //gfx.print("DIT IS EEN TEST OM TEI ZIEN OF IE HET GOED OET");
+  //gfx.display();
   //gfx.fillScreen(TFT_WHITE);     // wis scherm
   //this->do_update_();            // ESPHome lambda
   //gfx.display();                 // laat zien
@@ -32,13 +34,13 @@ void MyEpaperDisplay::update() {
 void MyEpaperDisplay::draw_absolute_pixel_internal(int x, int y, esphome::Color color) {
   ESP_LOGD("my_display", "TEST: draw_absolute_pixel_internal wordt aangeroepen");
   uint16_t col = color.is_on() ? TFT_BLACK : TFT_WHITE;
-  gfx.drawPixel(x, y, col);
+//  gfx.drawPixel(x, y, col);
 }
 
 void MyEpaperDisplay::fill(esphome::Color color) {
   ESP_LOGD("my_display", "prodedure fill aangeroepen");
   uint16_t col = color.is_on() ? 0x0000 : 0xFFFF;
-  gfx.fillScreen(col);
+//  gfx.fillScreen(col);
 }
 
 // === Verplichte overrides ===
