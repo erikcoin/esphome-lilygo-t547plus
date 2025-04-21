b4309c4fc3e74b3c5bc293f318a9fab57fd9bfdc
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 07:49:54 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -25,7 +25,7 @@ void MyEpaperDisplay::draw_absolute_pixel_internal(int x, int y, esphome::Color 
   gfx.drawPixel(x, y, col);
 }
 
-void MyEpaperDisplay::fill(Color color) {
+void MyEpaperDisplay::fill(esphome::Color color) {
   ESP_LOGD("my_display", "prodedure fill aangeroepen");
   uint16_t col = color.is_on() ? 0x0000 : 0xFFFF;
   gfx.fillScreen(col);
