41d27a13734a433851fec412fb42abe0d7e421fd
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 10:29:29 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -1,46 +1,18 @@
 #include "my_display.h"
-#include "esphome/core/log.h"
 
 namespace esphome {
-namespace my_display22 {
-
-static const char *const TAG = "my_display";
-
-MyEpaperDisplay::MyEpaperDisplay() {}
+namespace my_display {
 
 void MyEpaperDisplay::setup() {
-  bool ok = gfx_.begin();
-  ESP_LOGD("my_display", "gfx_.begin() resultaat: %s", ok ? "OK" : "MISLUKT");
-
-  gfx_.setRotation(0);
-  gfx_.fillScreen(TFT_WHITE);
-  gfx_.display();
-  lgfx::v1::delay(1000);
-  //delay(1000); // Wacht een seconde om het scherm te stabiliseren
-
-  gfx_.fillScreen(TFT_WHITE); // Voer een volledige refresh uit
-  gfx_.display();
+  gfx.begin();
+  gfx.setEpdMode(epd_mode_t::epd_quality);  // of epd_fast
+  gfx.fillScreen(TFT_WHITE);
+  gfx.display();  // push naar scherm
 }
 
 void MyEpaperDisplay::update() {
-  // Geen verdere acties tijdens update
+  // Niks doen, enkel voor compatibiliteit
 }
 
-void MyEpaperDisplay::draw_absolute_pixel_internal(int x, int y, Color color) {
-  // Geen implementatie nodig voor deze test
-}
-
-display::DisplayType MyEpaperDisplay::get_display_type() {
-  return display::DisplayType::DISPLAY_TYPE_GRAYSCALE;
-}
-
-int MyEpaperDisplay::get_width_internal() {
-  return 960;
-}
-
-int MyEpaperDisplay::get_height_internal() {
-  return 540;
-}
-
-}  // namespace my_display22
+}  // namespace my_display
 }  // namespace esphome
