f75070e3b6a662589b688a850d87ed01e9b0f9a4
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 16:45:07 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -1,35 +1,24 @@
-#pragma once
 
-#include "esphome/components/display/display_buffer.h"
+#pragma once
 #include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
 #include <M5GFX.h>
 
 namespace esphome {
 namespace my_display {
-int32_t x;
-int32_t y;
-class MyEpaperDisplay : public display::DisplayBuffer {
+
+class MyDisplay : public PollingComponent, public display::DisplayBuffer {
  public:
-  M5GFX gfx;
-  M5Canvas canvas;
   void setup() override;
   void update() override;
   void dump_config() override {}
 
-  int get_width() override { return 960; }
-  int get_height() override { return 540; }
+  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
+  void draw_absolute_pixel_internal(int x, int y, int color) override;
 
  protected:
-  void draw_absolute_pixel_internal(int x, int y, Color color) override {
-    // voorlopig geen individuele pixels tekenen
-  }
-
-  display::DisplayType get_display_type() override {
-    return display::DisplayType::DISPLAY_TYPE_GRAYSCALE;
-  }
-
-  int get_width_internal() override { return get_width(); }
-  int get_height_internal() override { return get_height(); }
+  M5GFX gfx_;
+  M5Canvas canvas_;
 };
 
 }  // namespace my_display
