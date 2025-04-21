cd0ce194dba949dacfe2bdd708f9ad45a7b668b1
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 09:00:17 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -19,12 +19,12 @@ public:
 
   void setup() override;
   //void update() override;
-  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
-  void fill(esphome::Color color) override;
+  //void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
+  //void fill(esphome::Color color) override;
 
   // Verplichte implementaties voor pure virtuals
-  //display::DisplayType get_display_type() override;
-  esphome::display::DisplayType get_display_type() override;
+  esphome::display::DisplayType get_display_type() override {
+    return esphome::display::DisplayType::DISPLAY_TYPE_GRAYSCALE;
 
   int get_width_internal() override;
   int get_height_internal() override;
