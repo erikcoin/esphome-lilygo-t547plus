45221bc5af22d8b36ead01b6f113f012c36ac990
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 17:08:20 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -1,25 +1,68 @@
-
 #pragma once
+
 #include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/version.h"
 #include "esphome/components/display/display_buffer.h"
-#include <M5GFX.h>
+
+#include "m5gfx.h"
+
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
 
 namespace esphome {
-namespace my_display {
+namespace t547 {
 
-class MyDisplay : public PollingComponent, public display::DisplayBuffer {
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2023, 12, 0)
+class T547 : public display::DisplayBuffer {
+#else
+class T547 : public PollingComponent, public display::DisplayBuffer {
+#endif  // VERSION_CODE(2023, 12, 0)
  public:
-  void setup() override;
-  void update() override;
-  void dump_config() override {}
+  void set_greyscale(bool greyscale) {
+    this->greyscale_ = greyscale;
+  }
 
-  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_COLOR; }
-  void draw_absolute_pixel_internal(int x, int y, int color) override;
+  float get_setup_priority() const override;
+
+  void dump_config() override;
+
+  void display();
+  void clean();
+  void update() override;
+
+  void setup() override;
+
+  uint8_t get_panel_state() { return this->panel_on_; }
+  bool get_greyscale() { return this->greyscale_; }
+
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2022,6,0)
+  display::DisplayType get_display_type() override {
+    return get_greyscale() ? display::DisplayType::DISPLAY_TYPE_GRAYSCALE : display::DisplayType::DISPLAY_TYPE_BINARY;
+  }
+#endif
 
  protected:
-  M5GFX gfx_;
-  M5Canvas canvas_;
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+
+  void eink_off_();
+  void eink_on_();
+
+
+  int get_width_internal() override { return 960; }
+
+  int get_height_internal() override { return 540; }
+
+  size_t get_buffer_length_();
+
+
+  uint8_t panel_on_ = 0;
+  uint8_t temperature_;
+
+  bool greyscale_;
+
 };
 
-}  // namespace my_display
+}  // namespace T547
 }  // namespace esphome
+
+#endif  // USE_ESP32_FRAMEWORK_ARDUINO
