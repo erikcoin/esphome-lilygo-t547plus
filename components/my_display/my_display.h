4dd76d4b5c32e8bc0c66ba19d9197ecc23302953
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 07:38:48 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -19,11 +19,11 @@ public:
 
   void setup() override;
   void update() override;
-  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
   void fill(Color color) override;
 
   // Verplichte implementaties voor pure virtuals
-  display::DisplayType get_display_type() override;
+  //display::DisplayType get_display_type() override;
   int get_width_internal() override;
   int get_height_internal() override;
 
