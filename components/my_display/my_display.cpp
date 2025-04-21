38bebf2d6470ffa7638a9deaafbdffb083979465
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 16:20:41 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -5,9 +5,9 @@ namespace my_display {
 
 void MyEpaperDisplay::setup() {
   gfx.begin();
-  gfx..fillScreen(TFT_BLACK);
-  x = display.width() / 2;
-  y = display.height() / 2;
+  gfx.fillScreen(TFT_BLACK);
+  x = gfx.width() / 2;
+  y = gfx.height() / 2;
   //gfx.setEpdMode(epd_mode_t::epd_fastest);
   canvas.createSprite(50, 50);
   canvas.fillSprite(TFT_WHITE);
@@ -15,9 +15,9 @@ void MyEpaperDisplay::setup() {
   canvas.println("M5Canvas");
 
   // Only the following process is actually drawn on the panel.
-  display.startWrite(); 
-  display.println("Display");
-  canvas.pushSprite(x, y);
+  gfx.startWrite(); 
+  gfx.println("Display");
+  gfx.pushSprite(x, y);
   display.endWrite();
   ESP_LOGI("my_display", "Scherm wit gemaakt bij opstart.");
 }
