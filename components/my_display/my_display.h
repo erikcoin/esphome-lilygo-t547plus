dd3d6b15c20eee9773ae833fede90d26591e58a8
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 08:57:53 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -18,7 +18,7 @@ public:
   MyEpaperDisplay();
 
   void setup() override;
-  void update() override;
+  //void update() override;
   void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
   void fill(esphome::Color color) override;
 
