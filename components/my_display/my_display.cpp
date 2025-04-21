fc03683b158e4c17b6ec5ea6b08752888f88f697
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 12:26:22 +0200

    Update my_display.cpp

--- components/my_display/my_display.cpp
+++ components/my_display/my_display.cpp
@@ -8,6 +8,7 @@ void MyEpaperDisplay::setup() {
   gfx.setEpdMode(epd_mode_t::epd_fastest);
   gfx.setRotation(0); // of 1/2/3 afhankelijk van hoe je scherm gemonteerd is
   gfx.fillScreen(TFT_WHITE);
+  gfx.clear();
   gfx.display();
   ESP_LOGI("my_display", "Scherm wit gemaakt bij opstart.");
 }
