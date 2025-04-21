4b6f66209df1fdb36f003ae63d94cc585ac1e3f6
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 07:18:09 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -21,7 +21,7 @@ public:
   void setup() override;
   void update() override;
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
-  void fill(esphome::Color color) override;
+  void fill(Color color) override;
 
   // Verplichte implementaties voor pure virtuals
   display::DisplayType get_display_type() override;
