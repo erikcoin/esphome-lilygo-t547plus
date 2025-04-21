96479c5521721633643b08431b7420bc53f0c167
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 12:09:21 +0200

    Update display.py

--- components/my_display/display.py
+++ components/my_display/display.py
@@ -14,5 +14,5 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    #await cg.register_component(var, config)
     await display.register_display(var, config)
