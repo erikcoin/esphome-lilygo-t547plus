3a6e1fff2c9cf41352c27f5cbe4d5d65878f0976
erikcoin <erikdijkstra@hotmail.com>
2025-04-21 09:34:22 +0200

    Update my_display.h

--- components/my_display/my_display.h
+++ components/my_display/my_display.h
@@ -1,37 +1,26 @@
 #pragma once
-#include "esphome.h"
-#include "esphome/core/component.h"
-#include "esphome/core/hal.h"
-#include "esphome/core/version.h"
-#include "esphome/components/display/display_buffer.h"
 
-//#include "epd_driver.h"
-//#include <epdiy.h>
+#include "esphome/core/component.h"
+#include "esphome/components/display/display_buffer.h"
 #include <M5GFX.h>
+
 namespace esphome {
 namespace my_display22 {
 
-//deze werkt:
-//class MyEpaperDisplay : public esphome::display::DisplayBuffer {
 class MyEpaperDisplay : public display::DisplayBuffer {
-public:
+ public:
   MyEpaperDisplay();
 
   void setup() override;
   void update() override;
-  void draw_absolute_pixel_internal(int x, int y, esphome::Color color) override;
-  //void fill(esphome::Color color) override;
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
 
-  // Verplichte implementaties voor pure virtuals
-  esphome::display::DisplayType get_display_type() override ;//{
- //   return esphome::display::DisplayType::DISPLAY_TYPE_GRAYSCALE;
- // }
+  display::DisplayType get_display_type() override;
   int get_width_internal() override;
   int get_height_internal() override;
 
  protected:
-  M5GFX gfx;
-  
+  M5GFX gfx_;
 };
 
 }  // namespace my_display22
