68514d408559ebe35a7145cf3fe97012d1cf3e73
erikcoin <erikdijkstra@hotmail.com>
2025-04-22 14:07:37 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -80,6 +80,7 @@ void M5PaperS3DisplayM5GFX::update() {
     ESP_LOGD(TAG, "Display lambda done.");
   }
   M5.Display.startWrite();
+  M5.Display.fillScreen(TFT_WHITE);
   M5.Display.fillRect(20, 480, 280, 60, TFT_WHITE);
   M5.Display.endWrite();
   M5.Display.startWrite();
