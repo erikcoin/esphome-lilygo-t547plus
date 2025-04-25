6425b76ed7b474f7bb0084aea3baba4fd765f938
erikcoin <erikdijkstra@hotmail.com>
2025-04-25 12:21:02 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -99,8 +99,8 @@ void M5PaperS3DisplayM5GFX::update() {
     M5.Display.waitDisplay();
   }
     this->do_update_();
-  this->M5.Display.display();
-  //gfx->display();
+  //this->M5.Display.display();
+  gfx->display();
 ////  M5.Display.startWrite();
 ////  //M5.Display.fillScreen(TFT_BLACK);
 ////  M5.Display.fillRect(20, 100, 280, 60, LIGHTGREY);
