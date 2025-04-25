d7e40baed3fc05d831f6458c567c41fd3d2e87b2
erikcoin <erikdijkstra@hotmail.com>
2025-04-25 14:25:53 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -92,10 +92,13 @@ void M5PaperS3DisplayM5GFX::update() {
 //M5.Display.display();
 ////  M5.Display.clearDisplay();
   if (this->writer_ != nullptr) {
+    ESP_LOGD(TAG, "maak wit...");
     this->canvas_.fillSprite(TFT_WHITE);  // begin met wit scherm
+    ESP_LOGD(TAG, "wit gemaakt nu pushing canvas...");
     this->canvas_.pushSprite(0, 0);       // push naar EPD
+    ESP_LOGD(TAG, "start writer...");
     this->writer_(*this);                 // lambda tekent op canvas
-    ESP_LOGD(TAG, "Lambda done, pushing canvas...");
+    ESP_LOGD(TAG, "Lambda writer done, pushing canvas...");
     this->canvas_.pushSprite(0, 0);       // push naar EPD
     //M5.Display.display();                 // e-paper refresh
    // M5.Display.waitDisplay();
