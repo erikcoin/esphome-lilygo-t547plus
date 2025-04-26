7e76e97eff7553e29127c9368017529d47a8af37
erikcoin <erikdijkstra@hotmail.com>
2025-04-26 12:21:57 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -137,7 +137,7 @@ void M5PaperS3DisplayM5GFX::draw_absolute_pixel_internal(int x, int y, Color col
     
    uint32_t native_color = get_native_m5gfx_color_(color);
    ESP_LOGD(TAG, "draw_pixel: (%d, %d, %d)", x, y,native_color);
-   this->gfx.drawPixel(x, y, native_color);
+   this->M5.Display.drawPixel(x, y, native_color);
 }
 
 // Zet esphome kleur om naar 4-bit grijswaarde (0-15)
