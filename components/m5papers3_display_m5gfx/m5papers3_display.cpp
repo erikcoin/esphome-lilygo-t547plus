b58a4534e3331759f59965b70b06f24ea34399fa
erikcoin <erikdijkstra@hotmail.com>
2025-04-27 17:12:18 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -58,7 +58,7 @@ void M5PaperS3DisplayM5GFX::update() {
     if (this->writer_ != nullptr) {
         ESP_LOGD(TAG, "Maak wit...");
        // this->canvas_.fillSprite(TFT_BLACK);
-     //   this->canvas_.fillSprite(TFT_WHITE);  // begin met wit scherm
+        this->canvas_.fillSprite(TFT_WHITE);  // begin met wit scherm
        // this->canvas_.setTextColor(TFT_BLACK);
         ESP_LOGD(TAG, "Start writer...");
         // Schrijf naar scherm met behulp van de lambda
