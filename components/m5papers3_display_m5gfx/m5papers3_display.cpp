8124e03ea1a55d787d87fa544515b80108b84475
erikcoin <erikdijkstra@hotmail.com>
2025-04-27 21:07:07 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -164,7 +164,7 @@ void M5PaperS3DisplayM5GFX::draw_absolute_pixel_internal(int x, int y, Color col
     uint16_t col = color.is_on() ? TFT_BLACK : TFT_WHITE;
     
    //uint32_t native_color = get_native_m5gfx_color_(color);
-  ESP_LOGD(TAG, "draw_pixel: (%d, %d, %d)", x, y,col);
+//  ESP_LOGD(TAG, "draw_pixel: (%d, %d, %d)", x, y,col);
 //voor schrijven naar scherm doe deze:
  //   M5.Display.drawPixel(x, y, col);
  //voor schrijven naar canvas doe deze:
