fee3734dcc716a4cee9aff9ecc44d4bd195a87c7
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 07:38:05 +0200

    Update touchscreen.py

--- components/Papertouch/touchscreen.py
+++ components/Papertouch/touchscreen.py
@@ -1,17 +1,15 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 import esphome.components.touchscreen as touchscreen
-from esphome.components import display
-from esphome import automation
 
 m5touch_ns = cg.esphome_ns.namespace('m5touch')
 M5TouchComponent = m5touch_ns.class_('M5TouchComponent', touchscreen.Touchscreen, cg.Component)
 
 CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend({}).extend(
-    cg.COMPONENT_SCHEMA
+    cv.COMPONENT_SCHEMA
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     #await cg.register_component(var, config)
     await touchscreen.register_touchscreen(var, config)
-
