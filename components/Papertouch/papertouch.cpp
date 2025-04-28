d88e92b34eb12a1abd315248c8ee9f7fd6691443
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 11:42:05 +0200

    Update papertouch.cpp

--- components/Papertouch/papertouch.cpp
+++ components/Papertouch/papertouch.cpp
@@ -1,10 +1,42 @@
 #include "papertouch.h"
+#include "esphome/core/log.h"
+#include "M5Unified.h"
 
 namespace esphome {
 namespace papertouch {
 
-// Your `setup()` and `update()` methods are implemented in the header file
-// No additional code is needed here for this example
+static const char *const TAG = "papertouch";
+
+void PaperTouch::setup() {
+  ESP_LOGI(TAG, "Setting up PaperTouch...");
+
+  // Init M5.Touch
+  if (!M5.Touch.isEnabled()) {
+    ESP_LOGW(TAG, "M5.Touch not enabled or GT911 not found!");
+  } else {
+    ESP_LOGI(TAG, "M5.Touch initialized successfully.");
+  }
+}
+
+void PaperTouch::loop() {
+  M5.update();
+}
+
+void PaperTouch::update_touches() {
+  this->touches_.clear();
+
+  if (!M5.Touch.isEnabled()) {
+    return;
+  }
+
+  uint16_t x, y;
+  if (M5.Touch.getTouch(&x, &y)) {
+    touchscreen::TouchPoint point;
+    point.x = x;
+    point.y = y;
+    this->touches_.push_back(point);
+  }
+}
 
 }  // namespace papertouch
 }  // namespace esphome
