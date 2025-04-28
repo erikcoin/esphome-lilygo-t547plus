113e70711945cee38b3ea9c329492c13f4e98067
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 11:19:07 +0200

    Update papertouch.cpp

--- components/Papertouch/papertouch.cpp
+++ components/Papertouch/papertouch.cpp
@@ -1,32 +1,10 @@
-#include "papertouch.h"
-#include "esphome/core/log.h"
+#include "touch_component.h"
 
 namespace esphome {
 namespace papertouch {
 
-static const char *const TAG = "papertouch";
-
-void papertouch::setup() {
-  ESP_LOGI(TAG, "Papertouch setup");
-
-  if (!M5.Touch.isEnabled()) {
-    ESP_LOGI(TAG, "Enabling M5.Touch");
-    M5.Touch.begin();
-  }
-}
-
-void papertouch::update_touches() {
-  if (M5.Touch.isEnabled() && M5.Touch.isPressed()) {
-    auto point = M5.Touch.getTouchPoint();
-    touchscreen::TouchPoint touch_point;
-    touch_point.x = point.x;
-    touch_point.y = point.y;
-    touch_point.id = 0;  // Je kan meer doen als je multitouch wilt
-    this->touch_points_.push_back(touch_point);
-
-    ESP_LOGD(TAG, "Touch at x=%d, y=%d", point.x, point.y);
-  }
-}
+// Your `setup()` and `update()` methods are implemented in the header file
+// No additional code is needed here for this example
 
 }  // namespace papertouch
 }  // namespace esphome
