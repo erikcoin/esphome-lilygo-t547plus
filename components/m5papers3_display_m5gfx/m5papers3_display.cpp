2eb1a0151fe558cbe3c4f629b001c98280db4111
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 12:15:54 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -128,6 +128,14 @@ void M5PaperS3DisplayM5GFX::draw_pixel_at(int x, int y, esphome::Color color) {
     uint16_t col = color.is_on() ? TFT_BLACK : TFT_WHITE;
     this->canvas_->drawPixel(x, y, col);
 }
-
+bool M5PaperS3DisplayM5GFX::get_touch(esphome::display::TouchPoint* point) {
+  touch_point_t tp[1];
+  if (M5.Display.getTouchRaw(tp, 1) > 0) {
+    point->x = tp[0].x;
+    point->y = tp[0].y;
+    return true;
+  }
+  return false;
+}
 } // namespace m5papers3_display_m5gfx
 } // namespace esphome
