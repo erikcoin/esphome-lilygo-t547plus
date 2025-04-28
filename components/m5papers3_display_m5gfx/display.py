40f18dcf4b4d8fbfe9dc2da6148101034464f663
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 17:08:04 +0200

    Update display.py

--- components/m5papers3_display_m5gfx/display.py
+++ components/m5papers3_display_m5gfx/display.py
@@ -1,6 +1,7 @@
 # my_components/m5papers3_display_m5gfx/display.py
 import esphome.codegen as cg
 import esphome.components.display as display
+from esphome.components import text_sensor
 import esphome.config_validation as cv
 #from esphome.components import display
 from esphome import automation
@@ -10,6 +11,7 @@ from esphome.const import (
     CONF_ROTATION,
     CONF_UPDATE_INTERVAL,
 )
+CONF_TOUCH_SENSOR = "touch_coordinates"
 AUTO_LOAD = ["display"]
 # Namespace voor onze C++ code
 # Gebruik een andere namespace om conflicten te vermijden als je beide hebt
@@ -22,6 +24,7 @@ M5PaperS3DisplayM5GFX = m5papers3_display_m5gfx_ns.class_(
 CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(M5PaperS3DisplayM5GFX),
+        cv.Optional(CONF_TOUCH_SENSOR): cv.use_id(text_sensor.TextSensor),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -35,7 +38,10 @@ async def to_code(config):
     # M5Unified is nodig voor M5.begin() die M5.Display configureert
    # cg.add_library("m5stack/M5Unified", None)
    # cg.add_library("m5stack/M5GFX", None)
-
+    if CONF_TOUCH_SENSOR in config:
+    touch_sensor = await cg.get_variable(config[CONF_TOUCH_SENSOR])
+    cg.add(var.set_touch_sensor(touch_sensor))
+    
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
             config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
@@ -47,3 +53,7 @@ async def to_code(config):
 
     if CONF_UPDATE_INTERVAL in config:
          cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
+
+    if CONF_TOUCH_SENSOR in config:
+        touch_sensor = await cg.get_variable(config[CONF_TOUCH_SENSOR])
+        cg.add(var.set_touch_sensor(touch_sensor))
