daff462cee5ba718850b769375ccc866633b0ce0
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 07:45:03 +0200

    Create papertouch.h

---
+++ components/Papertouch/papertouch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "esphome/components/touchscreen/touchscreen.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace papertouch {
+
+class PaperTouch : public touchscreen::Touchscreen, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void update_touches() override;
+};
+
+}  // namespace papertouch
+}  // namespace esphome
