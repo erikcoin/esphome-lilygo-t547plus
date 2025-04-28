aa22c8ee91f26f8b68088926da9d55fea77aff7f
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 09:32:58 +0200

    Update touchscreen.py

--- components/Papertouch/touchscreen.py
+++ components/Papertouch/touchscreen.py
@@ -20,7 +20,7 @@ async def to_code(config):
 
 
     
-    var = await touchscreen.register_touchscreen(var, config)
+    var = await touchscreen.register_touchscreen(config, Papertouch.new())
     #var = await cg.register_component(config, Papertouch.new())
     display_var = await cg.get_variable(config["display"])
     cg.add(var.set_display(display_var))
