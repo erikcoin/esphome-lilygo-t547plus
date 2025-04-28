ea8160bf4a21d3a934faf5dcaf4ae3e4c7b52fe2
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 07:58:37 +0200

    Update papertouch.h

--- components/Papertouch/papertouch.h
+++ components/Papertouch/papertouch.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include "esphome/components/touchscreen/touchscreen.h"
-#include "esphome/core/component.h"
+#include "esphome/components/display/display.h"
+
+#include <M5GFX.h>  // Zorg dat M5GFX beschikbaar is!
 
 namespace esphome {
 namespace papertouch {
 
-class PaperTouch : public touchscreen::Touchscreen, public Component {
+class Papertouch : public touchscreen::TouchscreenComponent {
  public:
   void setup() override;
-  void loop() override;
   void update_touches() override;
+
+  void set_display(display::Display* display) { this->display_ = display; }
+
+ protected:
+  display::Display* display_;
 };
 
 }  // namespace papertouch
