f525b27f78bf9ee1757d7af0adbbe12f78802dc3
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 11:18:52 +0200

    Update touchscreen.py

--- components/Papertouch/touchscreen.py
+++ components/Papertouch/touchscreen.py
@@ -3,14 +3,21 @@ import esphome.config_validation as cv
 from esphome.components import touchscreen
 from esphome.const import CONF_ID
 
+# Define the namespace where this component will live
 papertouch_ns = cg.esphome_ns.namespace('papertouch')
-PaperTouch = papertouch_ns.class_('PaperTouch', touchscreen.Touchscreen, cg.Component)
 
+# Link the C++ class (defined in the header) with the Python code
+Papertouch = papertouch_ns.class_('Papertouch', touchscreen.Touchscreen, cg.Component)
+
+# Define the configuration schema for the custom component
 CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend({
-    cv.Required(CONF_ID): cv.declare_id(PaperTouch),
+    cv.Required(CONF_ID): cv.declare_id(Papertouch),
 }).extend(cv.COMPONENT_SCHEMA)
 
+# This function converts the configuration into C++ code
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    #await cg.register_component(var, config)  # Register the component properly
+    # Register the component in ESPHome
+    await cg.register_component(var, config)
+    # Register the touchscreen in ESPHome (to handle touch events)
     await touchscreen.register_touchscreen(var, config)
