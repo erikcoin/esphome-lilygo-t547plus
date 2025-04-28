6098399895af2768b99b32b44a32b003be87a096
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 16:07:52 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -156,7 +156,7 @@ void M5PaperS3DisplayM5GFX::send_coordinates(TouchPoint tp) {
   ESP_LOGD("custom", "Sending coordinates: %s", coords.c_str());
 
   // Directly publish the coordinates to a defined text sensor
-  id(touch_coordinates).publish_state(coords);  // Ensure 'touch_coordinates' is defined in your YAML
+  id(esphome::touch_coordinates).publish_state(coords);  // Ensure 'touch_coordinates' is defined in your YAML
 }
 
 
