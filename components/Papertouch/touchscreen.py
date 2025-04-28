4d372dcb769bce358e32df3698517637ad2e605f
erikcoin <erikdijkstra@hotmail.com>
2025-04-28 10:29:18 +0200

    Update touchscreen.py

--- components/Papertouch/touchscreen.py
+++ components/Papertouch/touchscreen.py
@@ -1,20 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
-from esphome.components import display, touchscreen
+from esphome.components import touchscreen
+from esphome.const import CONF_ID
 
-Papertouch = cg.Component
+papertouch_ns = cg.esphome_ns.namespace('papertouch')
+PaperTouch = papertouch_ns.class_('PaperTouch', touchscreen.Touchscreen, cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Papertouch),
-    cv.Required("display"): cv.use_id(display.Display),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    # Registreer de component
-    var = cg.new_Papertouch()  # Dit moet het component correct instantiëren
-    await cg.register_component(var, config)
-
-    # Haal de display variable op en koppel deze aan het touchscreen
-    display_var = await cg.get_variable(config["display"])
-    var.set_display(display_var)  # Koppel de display aan het touchscreen component
+    var = cg.new_Pvariable(config[CONF_ID])
+    #await cg.register_component(var, config)
+    await touchscreen.register_touchscreen(var, config)
+    
