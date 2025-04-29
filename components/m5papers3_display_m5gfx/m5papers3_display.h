e1793b3d592402a2d14d3cb9fb509c6db3c0e0d0
erikcoin <erikdijkstra@hotmail.com>
2025-04-29 21:13:48 +0200

    Update m5papers3_display.h

--- components/m5papers3_display_m5gfx/m5papers3_display.h
+++ components/m5papers3_display_m5gfx/m5papers3_display.h
@@ -51,6 +51,8 @@ class M5PaperS3DisplayM5GFX : public display::Display {
   
  protected:
   text_sensor::TextSensor *touch_coordinates_sensor{nullptr};  // Sensor pointer
+  text_sensor::TextSensor *touch_x_sensor{nullptr};
+  text_sensor::TextSensor *touch_y_sensor{nullptr};
   void draw_absolute_pixel_internal(int x, int y, esphome::Color color);
   M5GFX gfx;
   lgfx::LGFX_Sprite *canvas_{nullptr};
