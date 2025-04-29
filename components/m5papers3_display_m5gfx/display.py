b222bb74e3019e718912f326be056e8e73f5f8c2
erikcoin <erikdijkstra@hotmail.com>
2025-04-30 00:25:44 +0200

    Update display.py

--- components/m5papers3_display_m5gfx/display.py
+++ components/m5papers3_display_m5gfx/display.py
@@ -72,9 +72,9 @@ async def to_code(config):
     #    touch_y = await cg.get_variable(config[CONF_TOUCH_Y_SENSOR])
     #    cg.add(var.set_touch_y_sensor(touch_y))
     if CONF_TOUCH_Y_SENSOR in config:
-        sens = yield sensor.new_sensor(config["touch_x"])
+        sens = await sensor.new_sensor(config["touch_x"])
         cg.call(mydisplay.set_touch_x_sensor)(sens)
 
     if CONF_TOUCH_Y_SENSOR in config:
-        sens = yield sensor.new_sensor(config["touch_y"])
+        sens = await sensor.new_sensor(config["touch_y"])
         cg.call(mydisplay.set_touch_y_sensor)(sens)
