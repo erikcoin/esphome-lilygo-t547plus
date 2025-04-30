5534c079d3559bc671958456d670b392a3bb07d0
erikcoin <erikdijkstra@hotmail.com>
2025-04-30 08:23:40 +0200

    Update m5papers3_display.cpp

--- components/m5papers3_display_m5gfx/m5papers3_display.cpp
+++ components/m5papers3_display_m5gfx/m5papers3_display.cpp
@@ -190,6 +190,9 @@ void M5PaperS3DisplayM5GFX::send_coordinates(TouchPoint tp) {
     std::string coords = "X: " + std::to_string(tp.x) + ", Y: " + std::to_string(tp.y);
     this->touch_coordinates_sensor->publish_state(coords);
     ESP_LOGD("custom", "Sending coordinates: %s", coords.c_str());
+      App.scheduler.set_timeout(200, [this]() {
+  this->touch_coordinates_sensor->publish_state("");
+    });
   } else {
     ESP_LOGW("custom", "Touch coordinates sensor not initialized!");
   }
