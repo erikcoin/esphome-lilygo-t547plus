6bd53c73c4b538f4f01c70660177174af21251f7
erikcoin <erikdijkstra@hotmail.com>
2025-05-02 09:43:37 +0200

    Update sensor.py

--- components/battery_sensor/sensor.py
+++ components/battery_sensor/sensor.py
@@ -1,10 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL, UNIT_VOLT, ICON_BATTERY, DEVICE_CLASS_VOLTAGE
+from esphome.const import (
+    CONF_ID,
+    UNIT_VOLT,
+    ICON_BATTERY,
+    DEVICE_CLASS_VOLTAGE,
+    CONF_UPDATE_INTERVAL,
+)
 
-my_battery_ns = cg.esphome_ns.namespace('my_battery')
-MyBatterySensor = my_battery_ns.class_('MyBatterySensor', sensor.Sensor, cg.PollingComponent)
+battery_sensor_ns = cg.esphome_ns.namespace('battery_sensor')
+MyBatterySensor = battery_sensor_ns.class_('MyBatterySensor', sensor.Sensor, cg.PollingComponent)
 
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_BATTERY, 2, DEVICE_CLASS_VOLTAGE).extend({
     cv.GenerateID(): cv.declare_id(MyBatterySensor),
