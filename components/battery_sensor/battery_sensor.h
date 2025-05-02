efdf5b10549d69fdf33b666626ebabb426cb5f30
erikcoin <erikdijkstra@hotmail.com>
2025-05-02 11:59:44 +0200

    Update battery_sensor.h

--- components/battery_sensor/battery_sensor.h
+++ components/battery_sensor/battery_sensor.h
@@ -5,31 +5,22 @@
 #include <M5Unified.h>
 
 namespace esphome {
-namespace battery_sensor {
+namespace battery_sensor  {
 
 class MyBatterySensor : public sensor::Sensor, public PollingComponent {
  public:
-  MyBatterySensor() : PollingComponent(60000) {}
+  MyBatterySensor() : PollingComponent(60000) {}  // elke 60s updaten
+
+  void setup() override {
+    // Zorg dat M5 al geïnitialiseerd is, dus M5.begin() moet al elders zijn aangeroepen
+  }
 
   void update() override {
-    float voltage = M5.Power.getBatteryVoltage() / 1000.0;  // millivolt naar volt
+    float voltage = M5.Power.getBatteryVoltage();  // in volt
     ESP_LOGD("battery", "Battery voltage: %.2f V", voltage);
     publish_state(voltage);
   }
 };
 
-class MyBatteryPercentageSensor : public sensor::Sensor, public PollingComponent {
- public:
-  MyBatteryPercentageSensor() : PollingComponent(60000) {}
-
-  void update() override {
-    float voltage = M5.Power.getBatteryVoltage() / 1000.0;
-    float percent = (voltage - 3.0f) / (4.2f - 3.0f) * 100.0f;
-    percent = std::max(0.0f, std::min(100.0f, percent));  // Clamp 0–100%
-    ESP_LOGD("battery", "Battery percentage: %.0f %%", percent);
-    publish_state(percent);
-  }
-};
-
-}  // namespace battery_sensor
+}  // namespace my_battery
 }  // namespace esphome
