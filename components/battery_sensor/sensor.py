70d765403d93a5432f05b4ab1d47d8cce04dd8c2
erikcoin <erikdijkstra@hotmail.com>
2025-05-02 09:46:44 +0200

    Update sensor.py

--- components/battery_sensor/sensor.py
+++ components/battery_sensor/sensor.py
@@ -11,8 +11,13 @@ from esphome.const import (
 
 battery_sensor_ns = cg.esphome_ns.namespace('battery_sensor')
 MyBatterySensor = battery_sensor_ns.class_('MyBatterySensor', sensor.Sensor, cg.PollingComponent)
-
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_BATTERY, 2, DEVICE_CLASS_VOLTAGE).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=UNIT_VOLT,
+    icon=ICON_BATTERY,
+    accuracy_decimals=2,
+    device_class=DEVICE_CLASS_VOLTAGE,
+).extend({
+#CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_BATTERY, 2, DEVICE_CLASS_VOLTAGE).extend({
     cv.GenerateID(): cv.declare_id(MyBatterySensor),
     cv.Optional(CONF_UPDATE_INTERVAL, default='60s'): cv.update_interval,
 })
