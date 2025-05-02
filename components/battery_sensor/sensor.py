39a5ae67d756cf63982df7ce1a3404ad13db005c
erikcoin <erikdijkstra@hotmail.com>
2025-05-02 11:46:48 +0200

    Update sensor.py

--- components/battery_sensor/sensor.py
+++ components/battery_sensor/sensor.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     CONF_ID,
+    CONF_NAME,
     UNIT_VOLT,
     UNIT_PERCENT,
     ICON_BATTERY,
@@ -15,23 +16,31 @@ battery_ns = cg.esphome_ns.namespace('battery_sensor')
 MyBatterySensor = battery_ns.class_('MyBatterySensor', sensor.Sensor, cg.PollingComponent)
 MyBatteryPercentageSensor = battery_ns.class_('MyBatteryPercentageSensor', sensor.Sensor, cg.PollingComponent)
 
+CONF_PERCENTAGE = "percentage"
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MyBatterySensor),
-    cv.Optional("percentage"): cv.declare_id(MyBatteryPercentageSensor),
+    cv.Optional(CONF_NAME, default="Battery Voltage"): cv.string,
+    cv.Optional(CONF_PERCENTAGE): cv.declare_id(MyBatteryPercentageSensor),
     cv.Optional(CONF_UPDATE_INTERVAL, default="60s"): cv.update_interval,
 })
-
 
 async def to_code(config):
     voltage = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(voltage, config)
-    await sensor.register_sensor(voltage, config)
+    await sensor.register_sensor(voltage, {
+        CONF_NAME: config[CONF_NAME],
+        "unit_of_measurement": UNIT_VOLT,
+        "icon": ICON_BATTERY,
+        "accuracy_decimals": 2,
+        "device_class": DEVICE_CLASS_VOLTAGE,
+    })
 
-    if "percentage" in config:
-        percent = cg.new_Pvariable(config["percentage"])
+    if CONF_PERCENTAGE in config:
+        percent = cg.new_Pvariable(config[CONF_PERCENTAGE])
         await cg.register_component(percent, config)
         await sensor.register_sensor(percent, {
-            CONF_ID: config["percentage"],
+            CONF_NAME: "Battery Percentage",
             "unit_of_measurement": UNIT_PERCENT,
             "icon": ICON_BATTERY,
             "accuracy_decimals": 0,
