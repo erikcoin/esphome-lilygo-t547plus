f38e30708622beb126cfa5cfd94e6b6eb656ea4f
erikcoin <erikdijkstra@hotmail.com>
2025-05-04 16:34:55 +0200

    Update display.py

--- components/papers33/display.py
+++ components/papers33/display.py
@@ -38,7 +38,7 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    #await cg.register_component(var, config)
     # *** Register using display.register_display ***
     await display.register_display(var, config)
 
