9523baafe34256737b0ec95d5847bd30bf8c57a3
erikcoin <erikdijkstra@hotmail.com>
2025-05-04 14:03:31 +0200

    Update papers33.cpp

--- components/papers33/papers33.cpp
+++ components/papers33/papers33.cpp
@@ -76,7 +76,7 @@ void M5PaperS3DisplayM5GFX::setup() {
     }
     ESP_LOGD(TAG, "Calculate required memory for the sprite buffer");
     // Calculate required memory for the sprite buffer
-    size_t required_bytes = (size_t)gfx.width() * gfx.height() * this->canvas_->getColorDepth() / 8;
+    //size_t required_bytes = (size_t)gfx.width() * gfx.height() * this->canvas_->getColorDepth() / 8;
     ESP_LOGD(TAG, "Attempting to create canvas sprite %d x %d @ %d bits", gfx.width(), gfx.height(), this->canvas_->getColorDepth());
     ESP_LOGD(TAG, "Estimated memory needed for sprite: %u bytes", required_bytes);
      // Log memory status before createSprite
