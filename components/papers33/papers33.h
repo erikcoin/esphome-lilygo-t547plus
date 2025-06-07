5903bd691c7a3ab6ab9714ea9df26949c99a0168
erikcoin <erikdijkstra@hotmail.com>
2025-06-07 08:16:51 +0200

    Update papers33.h

--- components/papers33/papers33.h
+++ components/papers33/papers33.h
@@ -56,12 +56,12 @@ class M5PaperS3DisplayM5GFX : public display::Display {
 //}
 //Trigger<> *make_button_trigger();
 
-//    Trigger<> *make_button_trigger(buttonid) { return &this->release_trigger_; }
+   Trigger<> *make_button_trigger() { return &this->release_trigger_; }
     void add_button(int x, int y, int width, int height, const std::string &buttonid, Trigger<> *trigger);//{
  //   ButtonConfig config = {x, y, width, height, id, trigger};
 //    this->buttons_.push_back(config);
 //}
-    //Trigger<> *get_on_press_trigger(int button_index);
+    
     void fill(Color color) override;
     int get_width_internal() override;
     int get_height_internal() override;
