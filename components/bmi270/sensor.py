37024e69ab601a72abd5cf6e093c2cfaafd27ed5
erikcoin <erikdijkstra@hotmail.com>
2025-06-08 21:55:12 +0200

    Update sensor.py

--- components/bmi270/sensor.py
+++ components/bmi270/sensor.py
@@ -1,11 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    UNIT_G,
-    ICON_ACCELERATION, ICON_ROTATE_3D
-)
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['sensor']
 AUTO_LOAD = ['m5unified']
@@ -17,33 +13,33 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BMI270Sensor),
 
     cv.Optional("accel_x"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_G,
-        icon=ICON_ACCELERATION,
+        unit_of_measurement="g",
+        icon="mdi:axis-x-arrow",
         accuracy_decimals=3
     ),
     cv.Optional("accel_y"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_G,
-        icon=ICON_ACCELERATION,
+        unit_of_measurement="g",
+        icon="mdi:axis-y-arrow",
         accuracy_decimals=3
     ),
     cv.Optional("accel_z"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_G,
-        icon=ICON_ACCELERATION,
+        unit_of_measurement="g",
+        icon="mdi:axis-z-arrow",
         accuracy_decimals=3
     ),
     cv.Optional("gyro_x"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
-        icon=ICON_ROTATE_3D,
+        unit_of_measurement="°/s",
+        icon="mdi:rotate-right",
         accuracy_decimals=1
     ),
     cv.Optional("gyro_y"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
-        icon=ICON_ROTATE_3D,
+        unit_of_measurement="°/s",
+        icon="mdi:rotate-right",
         accuracy_decimals=1
     ),
     cv.Optional("gyro_z"): sensor.sensor_schema(
-        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
-        icon=ICON_ROTATE_3D,
+        unit_of_measurement="°/s",
+        icon="mdi:rotate-right",
         accuracy_decimals=1
     ),
 }).extend(cv.polling_component_schema("1s"))
@@ -55,7 +51,7 @@ async def to_code(config):
     for axis in ['x', 'y', 'z']:
         if f"accel_{axis}" in config:
             sens = await sensor.new_sensor(config[f"accel_{axis}"])
-            cg.add(getattr(var, f"accel_{axis}_sensor", None).set(sens))
+            cg.add(getattr(var, f"accel_{axis}_sensor").set(sens))
         if f"gyro_{axis}" in config:
             sens = await sensor.new_sensor(config[f"gyro_{axis}"])
-            cg.add(getattr(var, f"gyro_{axis}_sensor", None).set(sens))
+            cg.add(getattr(var, f"gyro_{axis}_sensor").set(sens))
