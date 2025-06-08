b75e06032162f4cd6bb59ecaf8bb8389e69f3a0b
erikcoin <erikdijkstra@hotmail.com>
2025-06-08 21:34:55 +0200

    Create sensor.py

---
+++ components/bmi270/sensor.py
@@ -0,0 +1,62 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import (
+    CONF_ID, CONF_ACCELERATION,
+    CONF_GYROSCOPE,
+    UNIT_G, UNIT_DEGREES_PER_SECOND,
+    ICON_ACCELERATION, ICON_ROTATE_3D
+)
+
+DEPENDENCIES = ['sensor']
+AUTO_LOAD = ['m5unified']
+
+bmi270_ns = cg.namespace('bmi270_sensor')
+BMI270Sensor = bmi270_ns.class_('BMI270Sensor', cg.PollingComponent)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(BMI270Sensor),
+
+    cv.Optional("accel_x"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_G,
+        icon=ICON_ACCELERATION,
+        accuracy_decimals=3
+    ),
+    cv.Optional("accel_y"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_G,
+        icon=ICON_ACCELERATION,
+        accuracy_decimals=3
+    ),
+    cv.Optional("accel_z"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_G,
+        icon=ICON_ACCELERATION,
+        accuracy_decimals=3
+    ),
+    cv.Optional("gyro_x"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
+        icon=ICON_ROTATE_3D,
+        accuracy_decimals=1
+    ),
+    cv.Optional("gyro_y"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
+        icon=ICON_ROTATE_3D,
+        accuracy_decimals=1
+    ),
+    cv.Optional("gyro_z"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_DEGREES_PER_SECOND,
+        icon=ICON_ROTATE_3D,
+        accuracy_decimals=1
+    ),
+}).extend(cv.polling_component_schema("1s"))
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    for axis in ['x', 'y', 'z']:
+        if f"accel_{axis}" in config:
+            sens = await sensor.new_sensor(config[f"accel_{axis}"])
+            cg.add(getattr(var, f"accel_{axis}_sensor", None).set(sens))
+        if f"gyro_{axis}" in config:
+            sens = await sensor.new_sensor(config[f"gyro_{axis}"])
+            cg.add(getattr(var, f"gyro_{axis}_sensor", None).set(sens))
