ce3a9a661c4c601dea44b8b1fdaf42a33239488a
erikcoin <erikdijkstra@hotmail.com>
2025-06-29 21:30:55 +0200

    Update papers33.cpp

--- components/papers33/papers33.cpp
+++ components/papers33/papers33.cpp
@@ -460,13 +460,14 @@ void M5PaperS3DisplayM5GFX::draw_all_buttons() {
 void M5PaperS3DisplayM5GFX::press_button_effect(int index, int duration_ms) {
   if (index < 0 || index >= this->buttons_.size()) return;
 
- if (index = 0)   
+ if (index = 0)   {
 // To draw/update Button 1  
 button1Sprite->fillSprite(10);  // Gray fill
 //button1Sprite->drawCenterString("Click", btnWidth/2, btnHeight/2, 2);
 button1Sprite->drawCenterString("Click", 200/2, 80/2, 2);
 button1Sprite->pushSprite(50, 100);  // Push to screen at x=50, y=100
-else if (index =1)
+ }
+ else if (index =1) {
 // To draw/update Button 2
 button2Sprite->fillSprite(5);   // Darker fill
 button2Sprite->drawCenterString("Reset", 200/2, 80/2, 2);
@@ -475,7 +476,8 @@ button2Sprite->pushSprite(300, 100); // Push to screen at x=300, y=100
 //  //draw_all_buttons();
 //  //  draw_button(index);
 //  canvas_->pushSprite(0, 0);
-else
+ }
+else return;
  // set_timeout(duration_ms, [this, index]() {
  //   this->buttons_[index].is_pressed = false;
  //   //draw_all_buttons();
