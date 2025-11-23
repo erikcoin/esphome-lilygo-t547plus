ad439788a511770616990fe8bdf572196bed893f
erikcoin <erikdijkstra@hotmail.com>
2025-11-23 17:16:35 +0100

    Update m5papers3_lvgl.cpp

--- components/m5papers3_lvgl/m5papers3_lvgl.cpp
+++ components/m5papers3_lvgl/m5papers3_lvgl.cpp
@@ -208,7 +208,7 @@ void M5PaperS3DisplayM5GFX::draw_pixel_at(int x, int y, Color color) {
                  (color.blue >> 3);
 
     // Draw using M5GFX
-    this->gfx_->drawPixel(x, y, c);
+    this->gfx_.drawPixel(x, y, c);
 }
 
 int M5PaperS3DisplayM5GFX::get_width_internal() {
