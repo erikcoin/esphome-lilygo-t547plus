dec2b2e59f77330185f814bc2b8061c799a8b62e
erikcoin <erikdijkstra@hotmail.com>
2025-11-23 12:54:39 +0100

    Update display.py

--- components/m5papers3_lvgl/display.py
+++ components/m5papers3_lvgl/display.py
@@ -9,16 +9,9 @@ from esphome.const import (
     CONF_LAMBDA,
     CONF_ROTATION,
     CONF_UPDATE_INTERVAL,
-    CONF_X_GRID,
-    CONF_Y_GRID,
     CONF_WIDTH,
     CONF_HEIGHT,
-    CONF_ON_PRESS,
 )
-
-CONF_TOUCH_SENSOR = "touch_coordinates"
-CONF_BUTTONS = "buttons"
-CONF_BUTTON_ID = "buttonid"
 
 DEPENDENCIES = ["network"]
 AUTO_LOAD = ["display", "text_sensor", "binary_sensor"]
@@ -30,21 +23,17 @@ M5PaperS3DisplayM5GFX = m5papers3_display_m5gfx_ns.class_(
 )
 
 BUTTON_SCHEMA = cv.Schema({
-    cv.Required(CONF_X_GRID): cv.int_range(min=0),
-    cv.Required(CONF_Y_GRID): cv.int_range(min=0),
+
     cv.Required(CONF_WIDTH): cv.int_range(min=1),
     cv.Required(CONF_HEIGHT): cv.int_range(min=1),
-    cv.Required(CONF_BUTTON_ID): cv.string,
-    # Use validate_automation directly
-    cv.Optional(CONF_ON_PRESS): validate_automation(single=True),
+
 })
 
 CONFIG_SCHEMA = cv.All(
     display.FULL_DISPLAY_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(M5PaperS3DisplayM5GFX),
-            cv.Optional(CONF_TOUCH_SENSOR): cv.use_id(text_sensor.TextSensor),
-            cv.Optional(CONF_BUTTONS): cv.ensure_list(BUTTON_SCHEMA),
+
         }
     ).extend(cv.COMPONENT_SCHEMA),
 )
@@ -65,28 +54,4 @@ async def to_code(config):
         if isinstance(rotation_val, str):
             rotation_val = display.DISPLAY_ROTATIONS[rotation_val]
         cg.add(var.set_rotation(rotation_val))
-
-    if CONF_TOUCH_SENSOR in config:
-        touch_sens = await cg.get_variable(config[CONF_TOUCH_SENSOR])
-        cg.add(var.set_touch_sensor(touch_sens))
-
-    if CONF_BUTTONS in config:
-        for button_config in config[CONF_BUTTONS]:
-            trigger = None
-            if CONF_ON_PRESS in button_config:
-                trigger = var.make_button_trigger(button_config[CONF_BUTTON_ID])
-                await build_automation(trigger, [], button_config[CONF_ON_PRESS])
-
-            cg.add(var.add_button(
-                button_config[CONF_X_GRID],
-                button_config[CONF_Y_GRID],
-                button_config[CONF_WIDTH],
-                button_config[CONF_HEIGHT],
-                button_config[CONF_BUTTON_ID],
-                trigger or cg.nullptr
-            ))
-            print("5Button trigger created:", trigger)
-
-    
-
-    cg.add_define("USE_M5PAPER_S3_M5GFX")
+        cg.add_define("USE_M5PAPER_S3_M5GFX")
