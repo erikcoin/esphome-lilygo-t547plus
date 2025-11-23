b8a58f533910014a9b3854c8c9109422961877ca
erikcoin <erikdijkstra@hotmail.com>
2025-11-23 12:26:55 +0100

    Update papers33.cpp

--- components/papers33/papers33.cpp
+++ components/papers33/papers33.cpp
@@ -544,7 +544,8 @@ void M5PaperS3DisplayM5GFX::lvgl_flush(const lv_area_t *area, lv_color_t *color_
   //this->M5.Display.display(); // or whatever your method is to update the EPD
    //     this->M5.Display.display();
   // Let LVGL know we’re done
-this->writer_(*this); 
+this->update();
+    
     ESP_LOGD(TAG, "voor flushready ");
   lv_disp_flush_ready(&disp_drv_);
 }
