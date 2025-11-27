53b1a75d26ab344b22e687c00cf96cba96cb8b63
erikcoin <erikdijkstra@hotmail.com>
2025-11-27 23:31:48 +0100

    Update m5papers3_lvgl.h

--- components/m5papers3_lvgl/m5papers3_lvgl.h
+++ components/m5papers3_lvgl/m5papers3_lvgl.h
@@ -48,7 +48,8 @@ private:
 
 
 // LVGL-related:
-  void lvgl_flush(const lv_area_t *area, lv_color_t *color_p);
+void lvgl_flush(lv_disp_drv_t* disp, const lv_area_t* area, lv_color_t* color_p);
+ // void lvgl_flush(const lv_area_t *area, lv_color_t *color_p);
   // override to run lv handler
  // void update() override;
 
