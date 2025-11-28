1b06c23def4dc8f0215cc8b4747a5b3c9e839dce
erikcoin <erikdijkstra@hotmail.com>
2025-11-28 16:18:17 +0100

    Update lvglai.h

--- components/lvglai/lvglai.h
+++ components/lvglai/lvglai.h
@@ -1,11 +1,6 @@
 #pragma once
 
 #include "esphome.h"
-#include <M5Unified.h>
-#include <M5GFX.h>
-#include "esphome/core/component.h"
-#include "esphome/components/display/display_buffer.h"
-#include "esphome/components/touchscreen/touchscreen.h"
 
 // LovyanGFX forward (we include proper header in cpp)
 namespace lgfx {
@@ -17,18 +12,15 @@ class LGFX_Sprite;
 namespace esphome {
 namespace m5papers3_display_m5gfx {
 
-class M5PaperS3DisplayM5GFX : public esphome::display::Display, public esphome::touchscreen::Touchscreen {
+class M5PaperS3DisplayM5GFX : public esphome::display::Display {
  public:
   void setup() override;
   void update() override;
-  ~M5PaperS3DisplayM5GFX(); //override;
-bool read_touch(esphome::touchscreen::TouchPoint *tp); // override;
+  ~M5PaperS3DisplayM5GFX() override;
+
   // Required by Display base class
   void draw_pixel_at(int x, int y, esphome::Color color) override;
-    // NEW REQUIRED OVERRIDES
-  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_BINARY; }
-  int get_width_internal() override { return 540; }   // your panel width
-  int get_height_internal() override { return 960; }  // your panel height
+
  protected:
   // Canvas (LGFX sprite) allocated in setup
   lgfx::v1::LGFX_Sprite *canvas_{nullptr};
