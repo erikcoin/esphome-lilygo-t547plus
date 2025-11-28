f86af4a4a8d32621f84e28ae5c808c288cefc78a
erikcoin <erikdijkstra@hotmail.com>
2025-11-28 14:36:50 +0100

    Update display.py

--- components/lvglai/display.py
+++ components/lvglai/display.py
@@ -43,9 +43,9 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    //await cg.register_component(var, config)
+    # await cg.register_component(var, config)
     await display.register_display(var, config)
-    //await touchscreen.register_touchscreen(var, config)
+    # await touchscreen.register_touchscreen(var, config)
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
             config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
