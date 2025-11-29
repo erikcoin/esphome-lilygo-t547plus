f3e79e25dcbe6842756511311646f8a42c7421ef
erikcoin <erikdijkstra@hotmail.com>
2025-11-29 13:21:42 +0100

    Update display.py

--- components/lvglai/display.py
+++ components/lvglai/display.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.components.display as display
-from esphome.components import text_sensor
+from esphome.components import text_sensor, touchscreen
 # Corrected import for automation helpers:
 from esphome.automation import validate_automation, build_automation
 import esphome.config_validation as cv
@@ -19,7 +19,7 @@ AUTO_LOAD = ["display", "text_sensor", "binary_sensor"]
 m5papers3_display_m5gfx_ns = cg.esphome_ns.namespace("m5papers3_display_m5gfx")
 
 M5PaperS3DisplayM5GFX = m5papers3_display_m5gfx_ns.class_(
-    "M5PaperS3DisplayM5GFX", cg.Component, display.Display
+    "M5PaperS3DisplayM5GFX", cg.Component, display.Display, touchscreen.Touchscreen
 )
 
 BUTTON_SCHEMA = cv.Schema({
