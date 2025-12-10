9d07e339526ac3be1f5e1670ab65db9e995a48c4
erikcoin <erikdijkstra@hotmail.com>
2025-12-10 10:02:15 +0100

    Update lightsleep.py

--- components/lightsleep/lightsleep.py
+++ components/lightsleep/lightsleep.py
@@ -1,21 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_WAKEUP_PIN
 
 lightsleep_ns = cg.esphome_ns.namespace("lightsleep")
-LightSleepComponent = lightsleep_ns.class_(
-    "LightSleepComponent", cg.Component
-)
+LightSleepComponent = lightsleep_ns.class_("LightSleepComponent", cg.Component)
 
-CONF_WAKEUP_PIN = "wakeup_pin"
-CONF_WAKEUP_MINUTES = "wakeup_minutes"
+CONF_WAKE_ON_TOUCH = "wake_on_touch"
+CONF_TURN_OFF_DISPLAY = "turn_off_display"
+CONF_MIN_INACTIVE_TIME = "min_inactive_time"
+CONF_WAKE_EVERY = "wake_every"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LightSleepComponent),
 
     cv.Optional(CONF_WAKEUP_PIN): cv.int_,
-    cv.Optional(CONF_WAKEUP_MINUTES, default=0): cv.All(cv.int_, cv.Range(min=0)),
+    cv.Optional(CONF_WAKE_ON_TOUCH, default=True): cv.boolean,
+    cv.Optional(CONF_TURN_OFF_DISPLAY, default=False): cv.boolean,
+
+    # Duration types (seconds, ms, etc.)
+    cv.Optional(CONF_MIN_INACTIVE_TIME, default="30s"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_WAKE_EVERY, default="0s"): cv.positive_time_period_milliseconds,
 })
 
 def to_code(config):
@@ -25,4 +29,8 @@ def to_code(config):
     if CONF_WAKEUP_PIN in config:
         cg.add(var.set_wakeup_pin(config[CONF_WAKEUP_PIN]))
 
-    cg.add(var.set_wakeup_minutes(config.get(CONF_WAKEUP_MINUTES, 0)))
+    cg.add(var.set_wake_on_touch(config[CONF_WAKE_ON_TOUCH]))
+    cg.add(var.set_turn_off_display(config[CONF_TURN_OFF_DISPLAY]))
+
+    cg.add(var.set_min_inactive_time(config[CONF_MIN_INACTIVE_TIME].total_milliseconds))
+    cg.add(var.set_wake_every(config[CONF_WAKE_EVERY].total_milliseconds))
