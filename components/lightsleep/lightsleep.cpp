43b6442670ac9cf86e4473e859426218d379a5d0
erikcoin <erikdijkstra@hotmail.com>
2025-12-10 09:50:06 +0100

    Update lightsleep.cpp

--- components/lightsleep/lightsleep.cpp
+++ components/lightsleep/lightsleep.cpp
@@ -1,7 +1,32 @@
 #include "lightsleep.h"
 
 namespace esphome {
-namespace m5p3_light_sleep {
-// empty
-}  
+namespace lightsleep {
+
+void LightSleepComponent::setup() {
+  M5.begin();
+  this->last_wake_time_ = millis();
 }
+
+void LightSleepComponent::loop() {
+  // Wakeup timer
+  if (this->wakeup_minutes_ > 0) {
+    uint32_t interval = this->wakeup_minutes_ * 60 * 1000;
+    if ((millis() - this->last_wake_time_) > interval) {
+      this->last_wake_time_ = millis();
+      esp_light_sleep_start();
+      return;
+    }
+  }
+
+  // Touch wakeup or GPIO wakeup
+  if (this->wakeup_pin_ >= 0) {
+    gpio_wakeup_enable((gpio_num_t)this->wakeup_pin_, GPIO_INTR_LOW_LEVEL);
+    esp_sleep_enable_gpio_wakeup();
+    esp_light_sleep_start();
+    gpio_wakeup_disable((gpio_num_t)this->wakeup_pin_);
+  }
+}
+
+}  // namespace lightsleep
+}  // namespace esphome
