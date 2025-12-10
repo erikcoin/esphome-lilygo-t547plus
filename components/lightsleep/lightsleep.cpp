4687cc15eab62239e18b50584ea50eaf99de3b94
erikcoin <erikdijkstra@hotmail.com>
2025-12-10 10:02:42 +0100

    Update lightsleep.cpp

--- components/lightsleep/lightsleep.cpp
+++ components/lightsleep/lightsleep.cpp
@@ -5,26 +5,51 @@ namespace lightsleep {
 
 void LightSleepComponent::setup() {
   M5.begin();
-  this->last_wake_time_ = millis();
+  last_activity_ = millis();
+  last_wake_timer_ = millis();
 }
 
 void LightSleepComponent::loop() {
-  // Wakeup timer
-  if (this->wakeup_minutes_ > 0) {
-    uint32_t interval = this->wakeup_minutes_ * 60 * 1000;
-    if ((millis() - this->last_wake_time_) > interval) {
-      this->last_wake_time_ = millis();
-      esp_light_sleep_start();
-      return;
-    }
+  M5.update();
+
+  // Check for activity (touch)
+  if (M5.Touch.isEnabled() && M5.Touch.getCount() > 0) {
+    last_activity_ = millis();
   }
 
-  // Touch wakeup or GPIO wakeup
-  if (this->wakeup_pin_ >= 0) {
-    gpio_wakeup_enable((gpio_num_t)this->wakeup_pin_, GPIO_INTR_LOW_LEVEL);
+  const uint32_t now = millis();
+
+  // 1️⃣ Periodic wake-up timer
+  if (wake_every_ > 0 && (now - last_wake_timer_) >= wake_every_) {
+    last_wake_timer_ = now;
+    enter_light_sleep_();
+    return;
+  }
+
+  // 2️⃣ Inactivity timeout sleep
+  if ((now - last_activity_) >= min_inactive_time_) {
+    enter_light_sleep_();
+    last_activity_ = now;
+  }
+}
+
+void LightSleepComponent::enter_light_sleep_() {
+  if (turn_off_display_) {
+    M5.Display.sleep();
+  }
+
+  if (wake_on_touch_ && wakeup_pin_ >= 0) {
+    gpio_wakeup_enable((gpio_num_t) wakeup_pin_, GPIO_INTR_LOW_LEVEL);
     esp_sleep_enable_gpio_wakeup();
-    esp_light_sleep_start();
-    gpio_wakeup_disable((gpio_num_t)this->wakeup_pin_);
+  }
+
+  esp_light_sleep_start();
+
+  if (wakeup_pin_ >= 0)
+    gpio_wakeup_disable((gpio_num_t) wakeup_pin_);
+
+  if (turn_off_display_) {
+    M5.Display.wakeup();
   }
 }
 
