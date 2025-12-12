c01b88aed8a445ea487e80decedd123374702247
erikcoin <erikdijkstra@hotmail.com>
2025-12-12 08:59:30 +0100

    Update sensor.py

--- components/battery_sensor/sensor.py
+++ components/battery_sensor/sensor.py
@@ -26,3 +26,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
+    if CONF_UPDATE_INTERVAL in config:
+        cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL]))
