11a6e539516b8a5c0dde9e3862db0b2c2c093010
erikcoin <erikdijkstra@hotmail.com>
2025-12-28 00:10:56 +0100

    Update lvgaidirect.cpp

--- components/lvgaidirect/lvgaidirect.cpp
+++ components/lvgaidirect/lvgaidirect.cpp
@@ -275,7 +275,7 @@ void M5PaperS3DisplayM5GFX::loop() {
      esp_sleep_enable_gpio_wakeup();
   //   suppress_lvgl_input_ = true; 
    //  pending_wake_touch_ = false;
-    wifi.disconnect();
+    M5.WiFi.mode(WIFI_OFF);
    
    // esp_light_sleep_start();
      
