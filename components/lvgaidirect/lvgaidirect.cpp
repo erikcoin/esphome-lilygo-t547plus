3c8b4da152e1fec663241b4d3a655baa75d2a502
erikcoin <erikdijkstra@hotmail.com>
2026-01-08 21:31:56 +0100

    Update lvgaidirect.cpp

--- components/lvgaidirect/lvgaidirect.cpp
+++ components/lvgaidirect/lvgaidirect.cpp
@@ -5,9 +5,9 @@
 // include LovyanGFX + M5Unified (adjust if your project uses different header names)
 #include <M5Unified.h>        // gives M5.Display and LovyanGFX (M5Unified)
 //#include <lgfx/v1.h>         // LGFX Sprite type
-#include "esphome/components/wifi/wifi_component.h"
+//#include "esphome/components/wifi/wifi_component.h"
 #include <atomic>
-#include "esp_sleep.h"
+//#include "esp_sleep.h"
 
 namespace esphome {
 namespace m5papers3_display_m5gfx {
