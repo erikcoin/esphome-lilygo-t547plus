f417a2dd932a224990cef08d3e7ba7075a80b07e
erikcoin <erikdijkstra@hotmail.com>
2026-01-09 12:44:28 +0100

    Update lightsleep.py

--- components/lightsleep/lightsleep.py
+++ components/lightsleep/lightsleep.py
@@ -1,36 +1,35 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_WAKEUP_PIN
+from esphome.const import CONF_ID
+from esphome import pins
 
-lightsleep_ns = cg.esphome_ns.namespace("lightsleep")
-LightSleepComponent = lightsleep_ns.class_("LightSleepComponent", cg.Component)
+DEPENDENCIES = ["esp32"]
+CODEOWNERS = ["@custom"]
 
-CONF_WAKE_ON_TOUCH = "wake_on_touch"
-CONF_TURN_OFF_DISPLAY = "turn_off_display"
-CONF_MIN_INACTIVE_TIME = "min_inactive_time"
-CONF_WAKE_EVERY = "wake_every"
+light_sleep_ns = cg.esphome_ns.namespace("light_sleep")
+LightSleepComponent = light_sleep_ns.class_("LightSleepComponent", cg.Component)
+
+CONF_SLEEP_DURATION = "sleep_duration"
+CONF_RUN_DURATION = "run_duration"
+CONF_WAKEUP_PIN = "wakeup_pin"
+CONF_WAKEUP_LEVEL = "wakeup_level"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LightSleepComponent),
+    cv.Optional(CONF_SLEEP_DURATION, default="5min"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_RUN_DURATION, default="1min"): cv.positive_time_period_milliseconds,
+    cv.Optional(CONF_WAKEUP_PIN): pins.internal_gpio_input_pin_schema,
+    cv.Optional(CONF_WAKEUP_LEVEL, default=0): cv.int_range(min=0, max=1),
+}).extend(cv.COMPONENT_SCHEMA)
 
-    cv.Optional(CONF_WAKEUP_PIN): cv.int_,
-    cv.Optional(CONF_WAKE_ON_TOUCH, default=True): cv.boolean,
-    cv.Optional(CONF_TURN_OFF_DISPLAY, default=False): cv.boolean,
-
-    # Duration types (seconds, ms, etc.)
-    cv.Optional(CONF_MIN_INACTIVE_TIME, default="30s"): cv.positive_time_period_milliseconds,
-    cv.Optional(CONF_WAKE_EVERY, default="0s"): cv.positive_time_period_milliseconds,
-})
-
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-
+    await cg.register_component(var, config)
+    
+    cg.add(var.set_sleep_duration(config[CONF_SLEEP_DURATION]))
+    cg.add(var.set_run_duration(config[CONF_RUN_DURATION]))
+    
     if CONF_WAKEUP_PIN in config:
-        cg.add(var.set_wakeup_pin(config[CONF_WAKEUP_PIN]))
-
-    cg.add(var.set_wake_on_touch(config[CONF_WAKE_ON_TOUCH]))
-    cg.add(var.set_turn_off_display(config[CONF_TURN_OFF_DISPLAY]))
-
-    cg.add(var.set_min_inactive_time(config[CONF_MIN_INACTIVE_TIME].total_milliseconds))
-    cg.add(var.set_wake_every(config[CONF_WAKE_EVERY].total_milliseconds))
+        pin = await cg.gpio_pin_expression(config[CONF_WAKEUP_PIN])
+        cg.add(var.set_wakeup_pin(pin))
+        cg.add(var.set_wakeup_level(config[CONF_WAKEUP_LEVEL]))
