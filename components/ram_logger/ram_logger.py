92b26309dbaf96777005d70fc66797bc7c4195f9
erikcoin <erikdijkstra@hotmail.com>
2026-01-09 16:04:23 +0100

    Create ram_logger.py

---
+++ components/ram_logger/ram_logger.py
@@ -0,0 +1,14 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+ram_logger_ns = cg.esphome_ns.namespace("ram_logger")
+RamLogger = ram_logger_ns.class_("RamLogger", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(RamLogger),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
