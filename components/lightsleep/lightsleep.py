f9aab2643f66b97254903ba6bc5c73faa3dd1d33
erikcoin <erikdijkstra@hotmail.com>
2026-01-12 10:20:02 +0100

    Update lightsleep.py

--- components/lightsleep/lightsleep.py
+++ components/lightsleep/lightsleep.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_WAKEUP_PIN
+from esphome.components import gt911
 
 lightsleep_ns = cg.esphome_ns.namespace("lightsleep")
 LightSleepComponent = lightsleep_ns.class_("LightSleepComponent", cg.Component)
@@ -9,6 +10,7 @@ CONF_WAKE_ON_TOUCH = "wake_on_touch"
 CONF_TURN_OFF_DISPLAY = "turn_off_display"
 CONF_MIN_INACTIVE_TIME = "min_inactive_time"
 CONF_WAKE_EVERY = "wake_every"
+CONF_GT911 = "gt911"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LightSleepComponent),
@@ -16,7 +18,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_WAKEUP_PIN): cv.int_,
     cv.Optional(CONF_WAKE_ON_TOUCH, default=True): cv.boolean,
     cv.Optional(CONF_TURN_OFF_DISPLAY, default=False): cv.boolean,
-
+    cv.Optional(CONF_GT911): cv.use_id(gt911.GT911Component),
     # Duration types (seconds, ms, etc.)
     cv.Optional(CONF_MIN_INACTIVE_TIME, default="30s"): cv.positive_time_period_milliseconds,
     cv.Optional(CONF_WAKE_EVERY, default="0s"): cv.positive_time_period_milliseconds,
@@ -34,3 +36,7 @@ def to_code(config):
 
     cg.add(var.set_min_inactive_time(config[CONF_MIN_INACTIVE_TIME].total_milliseconds))
     cg.add(var.set_wake_every(config[CONF_WAKE_EVERY].total_milliseconds))
+    
+    if CONF_GT911 in config:
+    touch = await cg.get_variable(config[CONF_GT911])
+    cg.add(var.set_gt911(my_touchscreen))
